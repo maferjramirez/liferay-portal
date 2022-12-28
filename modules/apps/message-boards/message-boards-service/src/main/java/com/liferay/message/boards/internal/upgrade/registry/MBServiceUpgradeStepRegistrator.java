45f125276de8f3173e1bbfab206b9b58172b41e5
Nilton Vieira <nilton.vieira@liferay.com>
2022-12-28 16:19:15 -0300

    LPS-166909 upgrade step to populate urlCategory

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/upgrade/registry/MBServiceUpgradeStepRegistrator.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/upgrade/registry/MBServiceUpgradeStepRegistrator.java
@@ -19,6 +19,7 @@ import com.liferay.message.boards.internal.upgrade.v2_0_0.util.MBThreadTable;
 import com.liferay.message.boards.internal.upgrade.v3_0_0.MBMessageTreePathUpgradeProcess;
 import com.liferay.message.boards.internal.upgrade.v3_1_0.UrlSubjectUpgradeProcess;
 import com.liferay.message.boards.internal.upgrade.v6_3_0.util.MBSuspiciousActivityTable;
+import com.liferay.message.boards.internal.upgrade.v6_5_0.UrlCategoryUpgradeProcess;
 import com.liferay.message.boards.model.MBThread;
 import com.liferay.portal.kernel.service.ResourceActionLocalService;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
@@ -141,6 +142,8 @@ public class MBServiceUpgradeStepRegistrator implements UpgradeStepRegistrator {
 			"6.4.0", "6.4.1",
 			UpgradeProcessFactory.alterColumnType(
 				"MBSuspiciousActivity", "reason", "VARCHAR(255) null"));
+
+		registry.register("6.4.1", "6.5.0", new UrlCategoryUpgradeProcess());
 	}
 
 	@Reference
