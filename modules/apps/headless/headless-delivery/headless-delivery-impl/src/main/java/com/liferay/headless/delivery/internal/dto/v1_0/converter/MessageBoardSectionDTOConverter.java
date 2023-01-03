5a4f547f68c687064ea61b92bcfe4db83cab3d9f
Nilton Vieira <nilton.vieira@liferay.com>
2023-01-03 09:03:32 -0300

    LPS-104321 expose friendlyUrlPath in title

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/MessageBoardSectionDTOConverter.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/dto/v1_0/converter/MessageBoardSectionDTOConverter.java
@@ -55,6 +55,7 @@ public class MessageBoardSectionDTOConverter
 				dateCreated = mbCategory.getCreateDate();
 				dateModified = mbCategory.getModifiedDate();
 				description = mbCategory.getDescription();
+				friendlyUrlPath = mbCategory.getUrlCategory();
 				id = mbCategory.getCategoryId();
 				numberOfMessageBoardSections =
 					_mbCategoryService.getCategoriesCount(
