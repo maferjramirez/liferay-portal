a9411d8049ab4fe481913a9431c95992a5c13a0a
Brian I. Kim <il.kim@liferay.com>
2023-05-30 23:09:41 -0700

    COMMERCE-11517 Add listener to handle COMMERCE AccountEntryUserRel creations

---
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/model/listener/AccountEntryUserRelModelListener.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.model.listener;
+
+import com.liferay.account.model.AccountEntryUserRel;
+import com.liferay.commerce.util.CommerceAccountHelper;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Brian I. Kim
+ */
+@Component(service = ModelListener.class)
+public class AccountEntryUserRelModelListener
+	extends BaseModelListener<AccountEntryUserRel> {
+
+	@Override
+	public void onAfterCreate(AccountEntryUserRel accountEntryUserRel)
+		throws ModelListenerException {
+
+		try {
+			_commerceAccountHelper.addDefaultRoles(
+				accountEntryUserRel.getAccountUserId());
+		}
+		catch (PortalException portalException) {
+			_log.error(portalException);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AccountEntryUserRelModelListener.class);
+
+	@Reference
+	private CommerceAccountHelper _commerceAccountHelper;
+
+}
