91a4639dfa62e85c458a8316a6657812fc63929b
Simon.Jiang <simon.jiang@liferay.com>
2023-06-26 21:34:29 +0800

    LPS-188758 SF

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/task/VerifyProductTask.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/task/VerifyProductTask.java
@@ -5,6 +5,9 @@
 
 package com.liferay.gradle.plugins.workspace.task;
 
+import com.liferay.gradle.plugins.workspace.WorkspaceExtension;
+import com.liferay.gradle.util.Validator;
+
 import java.util.Objects;
 
 import org.gradle.api.DefaultTask;
@@ -13,10 +16,6 @@ import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
 
-import com.liferay.gradle.plugins.workspace.WorkspaceExtension;
-import com.liferay.gradle.plugins.workspace.WorkspaceExtension.ProductInfo;
-import com.liferay.gradle.util.Validator;
-
 /**
  * @author Simon Jiang
  */
@@ -24,85 +23,95 @@ public class VerifyProductTask extends DefaultTask {
 
 	@Input
 	@Optional
-	public String getProcut() {
-		return _product;
-	}
-
-	@Input
-	@Optional
 	public WorkspaceExtension getExtension() {
 		return _extension;
 	}
 
-	public void setProduct(String product) {
-		_product = product;
+	@Input
+	@Optional
+	public String getProcut() {
+		return _product;
 	}
 
 	public void setExtension(WorkspaceExtension extension) {
 		_extension = extension;
 	}
 
+	public void setProduct(String product) {
+		_product = product;
+	}
+
 	@TaskAction
 	public void verifyProduct() throws Exception {
-		ProductInfo productInfo = _extension.getProductInfo();
-		
+		WorkspaceExtension.ProductInfo productInfo =
+			_extension.getProductInfo();
+
 		if (Objects.isNull(productInfo)) {
 			throw new GradleException(
 				"Can not get produtInfo for product '" + _product + "'");
 		}
 
-		String appServerTomcatVersion = _extension.getAppServerTomcatVersion();
-		String defaultAppServerTomcatVersion = productInfo.getAppServerTomcatVersion();
-		
-		if (Objects.equals(appServerTomcatVersion, defaultAppServerTomcatVersion) 
-				&& Validator.isNull(defaultAppServerTomcatVersion)) {
+		String defaultAppServerTomcatVersion =
+			productInfo.getAppServerTomcatVersion();
+
+		if (Objects.equals(
+				_extension.getAppServerTomcatVersion(),
+				defaultAppServerTomcatVersion) &&
+			Validator.isNull(defaultAppServerTomcatVersion)) {
+
 			throw new GradleException(
 				"Can not get correct tomcat version for product '" + _product +
 					"'");
 		}
 
-		String bundleChecksumMD5 = _extension.getBundleChecksumMD5();
 		String defaultBundleChecksumMD5 = productInfo.getBundleChecksumMD5();
-		
-		if (Objects.equals(bundleChecksumMD5, defaultBundleChecksumMD5) 
-				&& Validator.isNull(defaultBundleChecksumMD5)) {
+
+		if (Objects.equals(
+				_extension.getBundleChecksumMD5(), defaultBundleChecksumMD5) &&
+			Validator.isNull(defaultBundleChecksumMD5)) {
+
 			throw new GradleException(
 				"Can not get correct bundleChecksumMD5 for product '" +
 					_product + "'");
 		}
 
-		String bundleUrl = _extension.getBundleUrl();
-		String defaultBundleUrl = productInfo.getBundleUrl();		
-		
-		if (Objects.equals(bundleUrl, defaultBundleUrl) 
-				&& Validator.isNull(defaultBundleUrl)) {
+		String defaultBundleUrl = productInfo.getBundleUrl();
+
+		if (Objects.equals(_extension.getBundleUrl(), defaultBundleUrl) &&
+			Validator.isNull(defaultBundleUrl)) {
+
 			throw new GradleException(
 				"Can not get correct bundle url for product '" + _product +
 					"'");
 		}
 
-		String dockerImage = _extension.getDockerImageLiferay();
-		String defaultLiferayDockerImage = productInfo.getLiferayDockerImage();	
-		
-		if (Objects.equals(dockerImage, defaultLiferayDockerImage) 
-				&& Validator.isNull(defaultLiferayDockerImage)) {
+		String defaultLiferayDockerImage = productInfo.getLiferayDockerImage();
+
+		if (Objects.equals(
+				_extension.getDockerImageLiferay(),
+				defaultLiferayDockerImage) &&
+			Validator.isNull(defaultLiferayDockerImage)) {
+
 			throw new GradleException(
 				"Can not get correct liferay docker image for product '" +
 					_product + "'");
 		}
 
-		String targetPlatformVersion = _extension.getTargetPlatformVersion();
-		String defaultTargetPlatformVersion = productInfo.getTargetPlatformVersion();			
-		
-		if (Objects.equals(targetPlatformVersion, defaultTargetPlatformVersion) 
-				&& Validator.isNull(defaultTargetPlatformVersion)) {
+		String defaultTargetPlatformVersion =
+			productInfo.getTargetPlatformVersion();
+
+		if (Objects.equals(
+				_extension.getTargetPlatformVersion(),
+				defaultTargetPlatformVersion) &&
+			Validator.isNull(defaultTargetPlatformVersion)) {
+
 			throw new GradleException(
 				"Can not get correct tareget platform version for product '" +
 					_product + "'");
 		}
 	}
 
-	private String _product;
 	private WorkspaceExtension _extension;
+	private String _product;
 
 }
