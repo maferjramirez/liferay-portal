9dae97ae9aff63d370b7414c0e44aacbb39f9bbb
Alan Huang <alan.huang@liferay.com>
2023-06-28 23:04:58 +0800

    LPS-188912 Add new ant task format-copyright

---
+++ modules/util/copyright-formatter/src/main/java/com/liferay/copyright/formatter/CopyrightFormatter.java
@@ -0,0 +1,248 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.copyright.formatter;
+
+import com.liferay.copyright.formatter.util.ArgumentsUtil;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.regex.Pattern;
+
+/**
+ * @author Alan Huang
+ */
+public class CopyrightFormatter {
+
+	public static void main(String[] args) throws Exception {
+		CopyrightFormatter copyrightFormatter = new CopyrightFormatter();
+
+		copyrightFormatter.formatCopyright(
+			ArgumentsUtil.getValue(args, "source.base.dir", "./"));
+	}
+
+	public void formatCopyright(String baseDirName) throws Exception {
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+		List<Future<Void>> futures = new CopyOnWriteArrayList<>();
+
+		for (File file : _getFiles(baseDirName)) {
+			Future<Void> future = executorService.submit(
+				new Callable<Void>() {
+
+					@Override
+					public Void call() throws Exception {
+						_formatCopyright(file);
+
+						return null;
+					}
+
+				});
+
+			futures.add(future);
+		}
+
+		for (Future<Void> future : futures) {
+			future.get();
+		}
+
+		executorService.shutdown();
+
+		while (!executorService.isTerminated()) {
+			Thread.sleep(20);
+		}
+	}
+
+	private void _formatCopyright(File file) throws Exception {
+		String content = new String(Files.readAllBytes(file.toPath()), "UTF-8");
+
+		content = _replaceCopyright(file.getAbsolutePath(), content);
+
+		FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+		fileOutputStream.write(content.getBytes());
+	}
+
+	private String _getFileCreationYear(String absolutePath) throws Exception {
+		Runtime runtime = Runtime.getRuntime();
+
+		Process process = null;
+
+		try {
+			String[] cmd = {
+				"sh", "-c",
+				"git log --follow --pretty=format:%as " + absolutePath +
+					" | tail -n1 | cut -c-4"
+			};
+
+			process = runtime.exec(cmd);
+		}
+		catch (IOException ioException) {
+			throw new Exception("Add Git to your PATH system variable first");
+		}
+
+		InputStreamReader inputStreamReader = new InputStreamReader(
+			process.getInputStream());
+
+		BufferedReader inputBufferedReader = new BufferedReader(
+			inputStreamReader);
+
+		return inputBufferedReader.readLine();
+	}
+
+	private List<File> _getFiles(String baseDirName) throws Exception {
+		List<File> files = new ArrayList<>();
+
+		Files.walkFileTree(
+			Paths.get(baseDirName),
+			new SimpleFileVisitor<Path>() {
+
+				@Override
+				public FileVisitResult preVisitDirectory(
+						Path dirPath, BasicFileAttributes basicFileAttributes)
+					throws IOException {
+
+					String dirName = String.valueOf(dirPath.getFileName());
+
+					List<String> skipDirNames = Arrays.asList(_SKIP_DIR_NAMES);
+
+					if (dirName.startsWith(".") ||
+						skipDirNames.contains(dirName)) {
+
+						return FileVisitResult.SKIP_SUBTREE;
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+				@Override
+				public FileVisitResult visitFile(
+						Path file, BasicFileAttributes basicFileAttributes)
+					throws IOException {
+
+					String fileName = String.valueOf(file.getFileName());
+
+					int x = fileName.lastIndexOf('.');
+
+					if (x == -1) {
+						return FileVisitResult.CONTINUE;
+					}
+
+					String fileExtension = fileName.substring(x);
+
+					List<String> fileExtensions = Arrays.asList(
+						_FILE_EXTENSIONS);
+
+					if (fileExtensions.contains(fileExtension)) {
+						files.add(file.toFile());
+					}
+
+					return FileVisitResult.CONTINUE;
+				}
+
+			});
+
+		return files;
+	}
+
+	private String _replaceCopyright(String absolutePath, String content)
+		throws Exception {
+
+		if (_copyright == null) {
+			ClassLoader classLoader = CopyrightFormatter.class.getClassLoader();
+
+			ByteArrayOutputStream byteArrayOutputStream =
+				new ByteArrayOutputStream();
+
+			InputStream inputStream = classLoader.getResourceAsStream(
+				"dependencies/copyright.txt");
+
+			byte[] bytes = new byte[1024];
+
+			while (true) {
+				int read = inputStream.read(bytes, 0, bytes.length);
+
+				if (read == -1) {
+					break;
+				}
+
+				byteArrayOutputStream.write(bytes, 0, read);
+			}
+
+			byteArrayOutputStream.flush();
+
+			_copyright = byteArrayOutputStream.toString();
+		}
+
+		if (_copyright == null) {
+			return content;
+		}
+
+		String year = _getFileCreationYear(absolutePath);
+
+		if (year == null) {
+			return content;
+		}
+
+		int x = content.indexOf("/**\n * Copyright");
+
+		if (x == -1) {
+			return content;
+		}
+
+		int y = content.indexOf("\n */");
+
+		if (y == -1) {
+			return content;
+		}
+
+		return content.substring(0, x) +
+			_copyright.replaceFirst(Pattern.quote("{$year}"), year) +
+				content.substring(y + 4);
+	}
+
+	private static final String[] _FILE_EXTENSIONS = {
+		".java", ".js", ".jsp", ".jspf", ".jsx"
+	};
+
+	private static final String[] _SKIP_DIR_NAMES = {
+		"bin", "build", "classes", "dependencies", "lib", "node_modules",
+		"node_modules_cache", "sql", "test-classes", "test-coverage",
+		"test-results", "tmp"
+	};
+
+	private String _copyright;
+
+}
