44076e5df8b7f94ccbb73eefcb385fc451f0ea23
EugenioAntunes <eugenio.antunes@liferay.com>
2023-06-28 11:38:04 -0300

    LPS-188083 Implementing Organization and Request status handler fragment

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/evp-organization-status-handler/index.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+const EVPOrganizationStatus = document.querySelector(
+	'.EVPOrganizationStatus div div div div div input'
+);
+EVPOrganizationStatus.setAttribute('value', 'Awaiting Approval On EVP');
+
+const requestStatus = document.querySelector('[name="organizationStatus"]');
+requestStatus.setAttribute('value', 'awaitingApprovalOnEvp');
+
+const organizationStatusLabel = document.querySelector(
+	'[name="organizationStatus-label"]'
+);
+organizationStatusLabel.setAttribute('value', 'Awaiting Approval On EVP');
