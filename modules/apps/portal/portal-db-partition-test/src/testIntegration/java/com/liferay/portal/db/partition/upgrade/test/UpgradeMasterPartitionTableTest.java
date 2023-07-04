0479fdd8a482c5e727a2339ddd678ec7115db3ca
Sofía Mendoza <sofia.mendoza@liferay.com>
2023-07-04 15:37:46 +0200

    LPS-185207 Add test for the new upgrade process

---
+++ modules/apps/portal/portal-db-partition-test/src/testIntegration/java/com/liferay/portal/db/partition/upgrade/test/UpgradeMasterPartitionTableTest.java
@@ -0,0 +1,130 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.db.partition.upgrade.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.db.partition.DBPartitionUtil;
+import com.liferay.portal.db.partition.test.util.BaseDBPartitionTestCase;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.upgrade.v7_4_x.UpgradeMasterPartitionTable;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Sofía Mendoza Gutiérrez
+ */
+@RunWith(Arquillian.class)
+public class UpgradeMasterPartitionTableTest extends BaseDBPartitionTestCase {
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		enableDBPartition();
+
+		addDBPartitions();
+
+		insertPartitionRequiredData();
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		deletePartitionRequiredData();
+
+		removeDBPartitions(false);
+
+		disableDBPartition();
+	}
+
+	@Test
+	public void testUpgrade() throws Exception {
+		try {
+			createAndPopulateTable(TEST_TABLE_NAME);
+
+			_createViewSQL(TEST_TABLE_NAME);
+
+			UpgradeProcess upgradeProcess = new UpgradeMasterPartitionTable(
+				TEST_TABLE_NAME);
+
+			upgradeProcess.upgrade();
+
+			DBPartitionUtil.forEachCompanyId(
+				companyId -> {
+					Assert.assertTrue(dbInspector.hasTable(TEST_TABLE_NAME));
+
+					try (PreparedStatement preparedStatement =
+							connection.prepareStatement(
+								"select count(1) from " + TEST_TABLE_NAME);
+						ResultSet resultSet =
+							preparedStatement.executeQuery()) {
+
+						int count = 0;
+
+						if (resultSet.next()) {
+							count = resultSet.getInt(1);
+						}
+
+						Assert.assertEquals(1, count);
+					}
+
+					try (PreparedStatement preparedStatement =
+							connection.prepareStatement(
+								StringBundler.concat(
+									"select testColumn from ", TEST_TABLE_NAME,
+									" where testColumn = ? and companyId = ",
+									"?"))) {
+
+						preparedStatement.setLong(1, 1L);
+						preparedStatement.setLong(2, _DEFAULT_COMPANY_ID);
+
+						try (ResultSet resultSet =
+								preparedStatement.executeQuery()) {
+
+							Assert.assertTrue(resultSet.next());
+						}
+					}
+				});
+		}
+		finally {
+			DBPartitionUtil.forEachCompanyId(
+				companyId -> dropTable(TEST_TABLE_NAME));
+		}
+	}
+
+	private void _createViewSQL(String viewName) throws Exception {
+		try (Statement statement = connection.createStatement()) {
+			String defaultSchemaName = connection.getCatalog();
+
+			DBPartitionUtil.forEachCompanyId(
+				companyId -> {
+					if (_DEFAULT_COMPANY_ID ==
+							DBPartitionUtil.getCurrentCompanyId()) {
+
+						return;
+					}
+
+					statement.execute(
+						StringBundler.concat(
+							"create or replace view ", getSchemaName(companyId),
+							StringPool.PERIOD, viewName, " as select * from ",
+							defaultSchemaName, StringPool.PERIOD, viewName));
+				});
+		}
+	}
+
+	private static final long _DEFAULT_COMPANY_ID =
+		CompanyThreadLocal.getCompanyId();
+
+}
