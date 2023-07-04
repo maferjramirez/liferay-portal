b53c144fda2d65b1940e24ee81b834d1c55081d0
Crescenzo Rega <crescenzo.rega@liferay.com>
2023-07-04 08:17:38 +0200

    COMMERCE-11854 Add new upgrade process

--- modules/apps/commerce/commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/internal/upgrade/registry/CommercePriceListServiceUpgradeStepRegistrator.java
+++ modules/apps/commerce/commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/internal/upgrade/registry/CommercePriceListServiceUpgradeStepRegistrator.java
@@ -154,6 +154,11 @@ public class CommercePriceListServiceUpgradeStepRegistrator
 			new com.liferay.commerce.price.list.internal.upgrade.v2_7_0.
 				CommercePriceEntryUpgradeProcess());
 
+		registry.register(
+			"2.7.0", "2.8.0",
+			new com.liferay.commerce.price.list.internal.upgrade.v2_8_0.
+				CommercePriceEntryUpgradeProcess());
+
 		if (_log.isInfoEnabled()) {
 			_log.info("Commerce price list upgrade step registrator finished");
 		}
