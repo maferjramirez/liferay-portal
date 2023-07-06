769a289bdcaf67b0183aa2d9db17568372018be9
Pablo Molina <contact@pablomolina.me>
2023-07-06 14:34:54 +0200

    LPS-189855 Migrate addFragmentComposition to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/actions/addFragmentComposition.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/actions/addFragmentComposition.ts
@@ -14,13 +14,27 @@
 
 import {ADD_FRAGMENT_COMPOSITION} from './types';
 
+export interface FragmentComposition {
+	fragmentCollectionId: string;
+	fragmentCollectionName: string;
+	fragmentEntryKey: string;
+	groupId: string;
+	icon: string;
+	imagePreviewURL: string;
+	name: string;
+	type: 'composition';
+}
+
 export default function addFragmentComposition({
 	fragmentCollectionId,
 	fragmentComposition,
+}: {
+	fragmentCollectionId: string;
+	fragmentComposition: FragmentComposition;
 }) {
 	return {
 		fragmentCollectionId,
 		fragmentComposition,
 		type: ADD_FRAGMENT_COMPOSITION,
-	};
+	} as const;
 }
