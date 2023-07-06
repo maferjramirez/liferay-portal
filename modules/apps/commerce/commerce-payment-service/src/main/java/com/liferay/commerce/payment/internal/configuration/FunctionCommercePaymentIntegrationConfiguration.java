dd181a3ced42321ba21a48ce73b5ea27011e1d92
Luca Pellizzon <luca.pellizzon@liferay.com>
2023-07-06 14:49:51 +0200

    COMMERCE-11180 client-extension configuration

---
+++ modules/apps/commerce/commerce-payment-service/src/main/java/com/liferay/commerce/payment/internal/configuration/FunctionCommercePaymentIntegrationConfiguration.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.payment.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Luca Pellizzon
+ */
+@ExtendedObjectClassDefinition(
+	category = "payment", factoryInstanceLabelAttribute = "key",
+	scope = ExtendedObjectClassDefinition.Scope.SYSTEM
+)
+@Meta.OCD(
+	factory = true,
+	id = "com.liferay.commerce.payment.internal.configuration.FunctionCommercePaymentIntegrationConfiguration"
+)
+public interface FunctionCommercePaymentIntegrationConfiguration {
+
+	@Meta.AD(type = Meta.Type.String)
+	public String authorizePath();
+
+	@Meta.AD(type = Meta.Type.String)
+	public String cancelPath();
+
+	@Meta.AD(type = Meta.Type.String)
+	public String capturePath();
+
+	@Meta.AD(name = "key")
+	public String key();
+
+	@Meta.AD(type = Meta.Type.String)
+	public String oAuth2ApplicationExternalReferenceCode();
+
+	@Meta.AD(type = Meta.Type.String)
+	public String refundPath();
+
+	@Meta.AD(type = Meta.Type.Integer)
+	public int type();
+
+	@Meta.AD(required = false, type = Meta.Type.String)
+	public String typeSettings();
+
+}
