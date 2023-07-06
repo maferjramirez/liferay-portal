dbe7528fa8774e22d8edd3c3e6a8fbe890ebb32f
Pablo Molina <contact@pablomolina.me>
2023-07-06 14:41:05 +0200

    LPS-189855 Migrate addFragmentEntrylinkComment to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/actions/addFragmentEntryLinkComment.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/actions/addFragmentEntryLinkComment.ts
@@ -14,15 +14,33 @@
 
 import {ADD_FRAGMENT_ENTRY_LINK_COMMENT} from './types';
 
+export interface FragmentEntryLinkComment {
+	author: {
+		fullName: string;
+		portraitURL: string;
+		userId: string;
+	};
+	body: string;
+	commentId: string;
+	dateDescription: string;
+	edited: false;
+	modifiedDateDescription: string;
+	resolved: false;
+}
+
 export default function addFragmentEntryLinkComment({
 	fragmentEntryLinkComment,
 	fragmentEntryLinkId,
 	parentCommentId,
+}: {
+	fragmentEntryLinkComment: FragmentEntryLinkComment;
+	fragmentEntryLinkId: string;
+	parentCommentId: string;
 }) {
 	return {
 		fragmentEntryLinkComment,
 		fragmentEntryLinkId,
 		parentCommentId,
 		type: ADD_FRAGMENT_ENTRY_LINK_COMMENT,
-	};
+	} as const;
 }
