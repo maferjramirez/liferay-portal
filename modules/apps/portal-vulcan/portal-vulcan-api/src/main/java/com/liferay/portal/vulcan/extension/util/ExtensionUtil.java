82fa955de9ee6681ba3a167a7ddf9d36244ddf1d
Carlos Correa <carlos.correa@liferay.com>
2023-07-06 14:38:09 +0200

    LPS-184046 Add methods to set and get the extended properties of a DTO

--- modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/extension/util/ExtensionUtil.java
+++ modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/extension/util/ExtensionUtil.java
@@ -14,12 +14,20 @@
 
 package com.liferay.portal.vulcan.extension.util;
 
+import com.liferay.petra.reflect.ReflectionUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.vulcan.extension.EntityExtensionHandler;
 import com.liferay.portal.vulcan.extension.ExtensionProvider;
 import com.liferay.portal.vulcan.extension.ExtensionProviderRegistry;
 
+import java.io.Serializable;
+
+import java.lang.reflect.Field;
+
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Carlos Correa
@@ -40,5 +48,35 @@ public class ExtensionUtil {
 
 		return new EntityExtensionHandler(className, extensionProviders);
 	}
+
+	public static Map<String, Serializable> getExtendedProperties(Object dto) {
+		try {
+			Field field = ReflectionUtil.getDeclaredField(
+				dto.getClass(), "_extendedProperties");
+
+			return (Map<String, Serializable>)field.get(dto);
+		}
+		catch (Exception exception) {
+			_log.error(exception);
+
+			return null;
+		}
+	}
+
+	public static void setExtendedProperties(
+		Object dto, Map<String, Serializable> extendedProperties) {
+
+		try {
+			Field field = ReflectionUtil.getDeclaredField(
+				dto.getClass(), "_extendedProperties");
+
+			field.set(dto, extendedProperties);
+		}
+		catch (Exception exception) {
+			_log.error(exception);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(ExtensionUtil.class);
 
 }
