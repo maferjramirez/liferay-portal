4a6454b4edb8f3789e15c7499d2095fed151f95a
Mateus Santana <mateus.santana@liferay.com>
2023-07-12 19:24:09 -0300

    LPS-190136 Create Object Validation Rule Spring Controller

---
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-etc-spring-boot/src/main/java/com/liferay/sample/ObjectValidationRule1RestController.java
@@ -0,0 +1,77 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.sample;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.json.JSONObject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mateus Santana
+ */
+@RequestMapping("/object/validation/rule/1")
+@RestController
+public class ObjectValidationRule1RestController extends BaseRestController {
+
+	@PostMapping
+	public ResponseEntity<String> post(
+		@AuthenticationPrincipal Jwt jwt, @RequestBody String json) {
+
+		log(jwt, _log, json);
+
+		return new ResponseEntity<>(
+			_validateObjectEntry(
+				json
+			).toString(),
+			HttpStatus.OK);
+	}
+
+	private JSONObject _getObjectEntryJSONObject(String json) {
+		JSONObject jsonObject = new JSONObject(json);
+
+		JSONObject objectEntryJSONObject = jsonObject.getJSONObject(
+			"objectEntry");
+
+		if (objectEntryJSONObject == null) {
+			throw new IllegalArgumentException("Object entry is null");
+		}
+
+		return objectEntryJSONObject;
+	}
+
+	private JSONObject _validateObjectEntry(String json) {
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("invalidFields", false);
+
+		JSONObject objectEntryJSONObject = _getObjectEntryJSONObject(json);
+
+		if (!objectEntryJSONObject.get(
+				"name"
+			).equals(
+				"Mateus"
+			)) {
+
+			return jsonObject.put("invalidFields", true);
+		}
+
+		return jsonObject;
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		ObjectValidationRule1RestController.class);
+
+}
