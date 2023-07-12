bb568b4e4ac0269471c36dba6dae8930fb1101c4
Bruno Santana <bruno.santana@liferay.com>
2023-07-12 17:26:03 -0300

    LPS-190026 add the redirect into the banner button

--- modules/apps/site-initializer/site-initializer-liferay-marketplace/src/main/resources/site-initializer/fragments/group/marketplace-base-fragments/fragments/app-banner/index.js
+++ modules/apps/site-initializer/site-initializer-liferay-marketplace/src/main/resources/site-initializer/fragments/group/marketplace-base-fragments/fragments/app-banner/index.js
@@ -13,17 +13,33 @@
  */
 
 Liferay.on("copy-link", () => {
-    const url = window.location.href;
-    navigator.clipboard.writeText(url)
-      Liferay.Util.openToast({message:"Copied link to the clipboard",type: "success"})
-  
-  })
-  
-  Liferay.on("contact-publisher", () => {
-    // eslint-disable-next-line no-undef
-    const emailAddress = `${publisherEmail}`;
-    const mailtoLink = `mailto: ${emailAddress}`;
-  
-    window.location.href = mailtoLink;
-  
-  })
+  const url = window.location.href;
+  navigator.clipboard.writeText(url)
+    Liferay.Util.openToast({message:"Copied link to the clipboard",type: "success"})
+
+})
+
+Liferay.on("contact-publisher", () => {
+  // eslint-disable-next-line no-undef
+  const emailAddress = `${publisherEmail}`;
+  const mailtoLink = `mailto: ${emailAddress}`;
+
+  window.location.href = mailtoLink;
+
+})
+
+Liferay.on("start-trial", () => {
+// eslint-disable-next-line no-undef
+const finalURL = `purchase-product-form?productId=${configuration.productID}`
+window.location.href = `${Liferay.ThemeDisplay.getPortalURL()}${getSiteURL()}/${finalURL}`;
+})
+
+const getSiteURL = () => {
+const layoutRelativeURL = Liferay.ThemeDisplay.getLayoutRelativeURL();
+
+if (layoutRelativeURL.includes('web')) {
+  return layoutRelativeURL.split('/').slice(0, 3).join('/');
+}
+
+return '';
+};
