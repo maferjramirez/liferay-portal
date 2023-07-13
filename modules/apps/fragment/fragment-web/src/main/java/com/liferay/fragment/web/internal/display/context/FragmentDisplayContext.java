8ece6752a65c6f809f52e9b655a56298607658b7
Victor Galan <victor.galan@liferay.com>
2023-07-13 12:13:45 +0000

    LPS-190469 Render new view when ff is enabled

--- modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/display/context/FragmentDisplayContext.java
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/display/context/FragmentDisplayContext.java
@@ -29,6 +29,7 @@ import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONUtil;
@@ -198,7 +199,23 @@ public class FragmentDisplayContext {
 					).add(
 						() -> hasManageFragmentEntriesPermission,
 						dropdownItem -> {
-							dropdownItem.putData("action", "openImportView");
+							if (FeatureFlagManagerUtil.isEnabled(
+									"LPS-174939")) {
+
+								dropdownItem.setHref(
+									PortletURLBuilder.createRenderURL(
+										_renderResponse
+									).setMVCRenderCommandName(
+										"/fragment/view_import"
+									).setParameter(
+										"fragmentCollectionId", 0
+									).buildString());
+							}
+							else {
+								dropdownItem.putData(
+									"action", "openImportView");
+							}
+
 							dropdownItem.setIcon("import");
 							dropdownItem.setLabel(
 								LanguageUtil.get(
