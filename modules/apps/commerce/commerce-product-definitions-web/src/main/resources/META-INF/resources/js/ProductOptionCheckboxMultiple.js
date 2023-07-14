125540b2edf08017706b0fc54df364603dcbd743
Alessio Rendina <asilcomando@gmail.com>
2023-07-14 15:43:08 +0200

    COMMERCE-9619 Refactor product option types js implementation

--- modules/apps/commerce/commerce-product-definitions-web/src/main/resources/META-INF/resources/js/ProductOptionCheckboxMultiple.js
+++ modules/apps/commerce/commerce-product-definitions-web/src/main/resources/META-INF/resources/js/ProductOptionCheckboxMultiple.js
@@ -4,51 +4,194 @@
  */
 
 import ClayForm, {ClayCheckbox} from '@clayui/form';
-import React, {useState} from 'react';
+import {useLiferayState} from '@liferay/frontend-js-state-web';
+import classnames from 'classnames';
+import skuOptionsAtom from 'commerce-frontend-js/utilities/atoms/skuOptionsAtom';
+import React, {useEffect, useState} from 'react';
 
 import Asterisk from './Asterisk';
+import {
+	getProductOptionName,
+	getSkuOptionsErrors,
+	initialSkuOptionsAtomState,
+	isRequired,
+} from './utils';
 
 const ProductOptionCheckboxMultiple = ({
-	label,
-	onChange,
-	productOptionValues,
-	required,
+	forceRequired,
+	namespace,
+	productOption,
 }) => {
-	const [options, setOptions] = useState(
-		productOptionValues.reduce((acc, cur) => {
-			acc[cur.id] = cur;
-
-			return acc;
-		}, {})
+	const [hasErrors, setHasErrors] = useState(false);
+	const [skuOptionsAtomState, setSkuOptionsAtomState] = useLiferayState(
+		skuOptionsAtom
 	);
 
-	const handleChange = ({target: {checked}}, id) => {
-		const updatedOptions = {
-			...options,
-			[id]: {...options[id], selected: checked},
+	const [productOptionValues, setProductOptionValues] = useState(
+		productOption.productOptionValues
+	);
+
+	useEffect(
+		() =>
+			setSkuOptionsAtomState({
+				...skuOptionsAtomState,
+				errors: getSkuOptionsErrors(
+					hasErrors,
+					productOption,
+					skuOptionsAtomState
+				),
+				namespace,
+			}),
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[hasErrors]
+	);
+
+	useEffect(() => {
+		let hasPreselected = false;
+		let initialSkuOptions = skuOptionsAtomState.skuOptions;
+
+		setProductOptionValues(
+			productOptionValues.map((productOptionValue) => {
+				if (productOptionValue.preselected) {
+					hasPreselected = true;
+
+					initialSkuOptions = [
+						...skuOptionsAtomState.skuOptions,
+						{
+							key: productOption.key,
+							skuOptionKey: productOption.key,
+							value: [productOptionValue.key],
+						},
+					];
+				}
+
+				return {
+					...productOptionValue,
+					selected: productOptionValue.preselected,
+				};
+			})
+		);
+
+		const required = productOption.required && !hasPreselected;
+
+		if (required) {
+			setHasErrors(true);
+		}
+
+		setSkuOptionsAtomState({
+			...skuOptionsAtomState,
+			errors: getSkuOptionsErrors(
+				required,
+				productOption,
+				skuOptionsAtomState
+			),
+			namespace,
+			skuOptions: initialSkuOptions,
+		});
+
+		return () => setSkuOptionsAtomState(initialSkuOptionsAtomState);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	const handleChange = ({target: {checked, value}}) => {
+		if (skuOptionsAtomState.updating) {
+			return;
+		}
+
+		setSkuOptionsAtomState({...skuOptionsAtomState, updating: true});
+
+		let currentSkuOptions = skuOptionsAtomState.skuOptions;
+
+		const curSkuOptionIndex = currentSkuOptions.findIndex(
+			(skuOption) => skuOption.skuOptionKey === productOption.key
+		);
+
+		const currentSkuOption = currentSkuOptions.filter(
+			(skuOption) => skuOption.skuOptionKey === productOption.key
+		)[0];
+
+		if (currentSkuOption) {
+			currentSkuOptions[curSkuOptionIndex] = {
+				key: productOption.key,
+				skuOptionKey: productOption.key,
+				value: checked
+					? [...currentSkuOptions[curSkuOptionIndex].value, value]
+					: currentSkuOptions[curSkuOptionIndex].value.filter(
+							(curVal) => !(curVal === value)
+					  ),
+			};
+		}
+		else {
+			currentSkuOptions = [
+				...currentSkuOptions,
+				{
+					key: productOption.key,
+					skuOptionKey: productOption.key,
+					value: [value],
+				},
+			];
+		}
+
+		const curProductOptionValueIndex = productOptionValues.findIndex(
+			(productOptionValue) => productOptionValue.key === value
+		);
+
+		productOptionValues[curProductOptionValueIndex] = {
+			...productOptionValues[curProductOptionValueIndex],
+			selected: checked,
 		};
 
-		setOptions(updatedOptions);
-		onChange(updatedOptions);
+		setProductOptionValues(productOptionValues);
+
+		const required =
+			(forceRequired || productOption.required) &&
+			currentSkuOptions[curSkuOptionIndex]?.value.length === 0;
+
+		setHasErrors(required);
+		setSkuOptionsAtomState({
+			...skuOptionsAtomState,
+			errors: getSkuOptionsErrors(
+				required,
+				productOption,
+				skuOptionsAtomState
+			),
+			skuOptions: currentSkuOptions,
+			updating: false,
+		});
 	};
 
 	return (
-		<ClayForm.Group>
+		<ClayForm.Group className={classnames({'has-error': hasErrors})}>
 			<label>
-				{label}
+				{getProductOptionName(productOption.name)}
 
-				<Asterisk required={required} />
+				<Asterisk
+					required={isRequired(
+						forceRequired,
+						skuOptionsAtomState.isAdmin,
+						productOption
+					)}
+				/>
 			</label>
 
-			{Object.values(options).map(({id, key, label, name, selected}) => (
+			{productOptionValues.map(({key, name, selected}) => (
 				<ClayCheckbox
 					checked={selected}
 					key={key}
-					label={label}
-					name={name}
-					onChange={(event) => handleChange(event, id)}
+					label={name}
+					name={key}
+					onChange={handleChange}
+					value={key}
 				/>
 			))}
+
+			{hasErrors && (
+				<ClayForm.FeedbackItem>
+					<ClayForm.FeedbackIndicator symbol="exclamation-full" />
+
+					{Liferay.Language.get('this-field-is-required')}
+				</ClayForm.FeedbackItem>
+			)}
 		</ClayForm.Group>
 	);
 };
--- modules/apps/commerce/commerce-product-definitions-web/src/main/resources/META-INF/resources/js/utils.js
+++ modules/apps/commerce/commerce-product-definitions-web/src/main/resources/META-INF/resources/js/utils.js
@@ -3,16 +3,98 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-const getInitialOption = (productOptionValues) => {
-	const selectedOption = productOptionValues.find(
-		(option) => option.selected === true
-	);
+import {isObject} from 'frontend-js-web/index';
 
-	if (selectedOption) {
-		return selectedOption;
+const getInitialProductOptionValue = (productOption) => {
+	if (!productOption.productOptionValues) {
+		return;
 	}
 
-	return productOptionValues.find((option) => option.defaultValue === true);
+	const selectedProductOptionValue = productOption.productOptionValues.find(
+		(productOptionValue) => productOptionValue.preselected === true
+	);
+
+	if (selectedProductOptionValue) {
+		return selectedProductOptionValue;
+	}
+
+	if (!productOption.skuContributor) {
+		return;
+	}
+
+	return productOption.productOptionValues[0];
 };
 
-export {getInitialOption};
+const getName = (
+	key,
+	name,
+	selectedProductOptionValueKey,
+	skuId,
+	relativePriceFormatted
+) => {
+	if (isObject(name)) {
+		name = name[Liferay.ThemeDisplay.getLanguageId()];
+
+		if (!name) {
+			name = name[Liferay.ThemeDisplay.getDefaultLanguageId()];
+		}
+	}
+
+	if (
+		selectedProductOptionValueKey === key ||
+		!skuId ||
+		!relativePriceFormatted
+	) {
+		return name;
+	}
+
+	return name + relativePriceFormatted;
+};
+
+const getProductOptionName = (name) => {
+	if (isObject(name)) {
+		name = name[Liferay.ThemeDisplay.getLanguageId()];
+
+		if (!name) {
+			name = name[Liferay.ThemeDisplay.getDefaultLanguageId()];
+		}
+	}
+
+	return name;
+};
+
+const getSkuOptionsErrors = (hasErrors, productOption, skuOptionsAtomState) =>
+	hasErrors
+		? [
+				...skuOptionsAtomState.errors,
+				{
+					hasErrors: true,
+					id: productOption.id,
+				},
+		  ]
+		: skuOptionsAtomState.errors.filter(
+				(error) => error.id !== productOption.id
+		  );
+
+const initialSkuOptionsAtomState = {
+	errors: [],
+	namespace: '',
+	skuOptions: [],
+	updating: false,
+};
+
+const isRequired = (forceRequired, isAdmin, productOption) =>
+	isAdmin
+		? forceRequired
+		: forceRequired ||
+		  productOption.required ||
+		  productOption.skuContributor;
+
+export {
+	getInitialProductOptionValue,
+	getName,
+	getProductOptionName,
+	getSkuOptionsErrors,
+	initialSkuOptionsAtomState,
+	isRequired,
+};
