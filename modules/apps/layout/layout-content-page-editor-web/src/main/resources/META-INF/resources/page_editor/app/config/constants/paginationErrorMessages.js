0379591c7d6d49c6073e9afca796ab978f08e9bd
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2023-07-14 11:26:18 +0200

    LPS-190344 Change the message when there is pagination

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/config/constants/paginationErrorMessages.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/config/constants/paginationErrorMessages.js
@@ -15,7 +15,7 @@
 export const PAGINATION_ERROR_MESSAGES = {
 	maximumItems: Liferay.Language.get('this-collection-has-x-items'),
 	maximumItemsPerPage: Liferay.Language.get(
-		'you-can-only-display-a-maximum-of-x-items-per-page'
+		'a-maximum-of-x-items-per-page-is-allowed'
 	),
 	noItems: Liferay.Language.get('this-collection-has-no-items'),
 };
