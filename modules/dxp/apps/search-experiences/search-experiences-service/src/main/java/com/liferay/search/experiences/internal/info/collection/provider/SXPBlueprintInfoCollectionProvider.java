2ca2d8c04bfc78bd8a81555780661e3c4775cb28
Felipe Lorenz <felipe.lorenz@liferay.com>
2023-07-17 10:30:57 -0300

    LPS-190166 Revert "LPS-176298 Uses transform util"
    This reverts commit 637d39e63ed30a7ce55fa26fe8c766ffae396369.

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/info/collection/provider/SXPBlueprintInfoCollectionProvider.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/info/collection/provider/SXPBlueprintInfoCollectionProvider.java
@@ -32,7 +32,6 @@ import com.liferay.info.localized.InfoLocalizedValue;
 import com.liferay.info.localized.bundle.ResourceBundleInfoLocalizedValue;
 import com.liferay.info.pagination.InfoPage;
 import com.liferay.info.pagination.Pagination;
-import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -162,7 +161,18 @@ public class SXPBlueprintInfoCollectionProvider
 		ServiceContext serviceContext =
 			ServiceContextThreadLocal.getServiceContext();
 
+		List<OptionInfoFieldType> optionInfoFieldTypes =
+			ListUtil.fromArray(
+				new OptionInfoFieldType(
+					true,
+					new ResourceBundleInfoLocalizedValue(
+						getClass(), "this-site"),
+					String.valueOf(serviceContext.getScopeGroupId())));
+
 		ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+
+		List<Group> groups = _groupLocalService.getActiveGroups(
+			themeDisplay.getCompanyId(), true);
 
 		User user = themeDisplay.getUser();
 
@@ -175,32 +185,19 @@ public class SXPBlueprintInfoCollectionProvider
 			_log.error(portalException);
 		}
 
-		List<Group> finalSiteGroups = siteGroups;
+		for (Group group : groups) {
+			if ((group == null) || group.isGuest() || !group.isSite() ||
+				((siteGroups != null) && !siteGroups.contains(group))) {
 
-		List<OptionInfoFieldType> optionInfoFieldTypes =
-			TransformUtil.transform(
-				_groupLocalService.getActiveGroups(
-					themeDisplay.getCompanyId(), true),
-				group -> {
-					if ((group == null) || group.isGuest() || !group.isSite() ||
-						((finalSiteGroups != null) &&
-						 !finalSiteGroups.contains(group))) {
+				continue;
+			}
 
-						return null;
-					}
-
-					return new OptionInfoFieldType(
-						new ResourceBundleInfoLocalizedValue(
-							getClass(), group.getNameCurrentValue()),
-						String.valueOf(group.getGroupId()));
-				});
-
-		optionInfoFieldTypes.add(
-			0,
-			new OptionInfoFieldType(
-				true,
-				new ResourceBundleInfoLocalizedValue(getClass(), "this-site"),
-				String.valueOf(serviceContext.getScopeGroupId())));
+			optionInfoFieldTypes.add(
+				new OptionInfoFieldType(
+					new ResourceBundleInfoLocalizedValue(
+						getClass(), group.getNameCurrentValue()),
+					String.valueOf(group.getGroupId())));
+		}
 
 		return optionInfoFieldTypes;
 	}
