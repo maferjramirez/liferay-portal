d8e58fcf3d09916f07100eb2f1cc90cad62ff4c9
Felipe Lorenz <felipe.lorenz@liferay.com>
2023-07-17 10:55:51 -0300

    LPS-190166 Revert "LPS-181903 LPS-188072 search-experiences-service: Implements getConfigurationInfoForm"
    This reverts commit 0cb1eb16881067fe1172e2ab6c73b3bf87fa708a.

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/service/impl/SXPBlueprintLocalServiceImpl.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/service/impl/SXPBlueprintLocalServiceImpl.java
@@ -25,7 +25,6 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.CompanyLocalService;
-import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.ResourceLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
@@ -244,7 +243,7 @@ public class SXPBlueprintLocalServiceImpl
 					InfoCollectionProvider.class,
 					new SXPBlueprintInfoCollectionProvider(
 						_assetHelper, _searcher, _searchRequestBuilderFactory,
-						sxpBlueprint, _groupLocalService),
+						sxpBlueprint),
 					HashMapDictionaryBuilder.<String, Object>put(
 						"company.id", sxpBlueprint.getCompanyId()
 					).put(
@@ -290,9 +289,6 @@ public class SXPBlueprintLocalServiceImpl
 
 	@Reference
 	private CompanyLocalService _companyLocalService;
-
-	@Reference
-	private GroupLocalService _groupLocalService;
 
 	@Reference
 	private Language _language;
