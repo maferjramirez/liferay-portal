47e6601a76568255f6a8e8f64ad43cdca76bd89b
Alan Huang <alan.huang@liferay.com>
2023-07-17 15:43:13 +0800

    LPS-190478 Auto SF

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/frontend/taglib/form/navigator/BaseLayoutFormNavigatorEntry.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/frontend/taglib/form/navigator/BaseLayoutFormNavigatorEntry.java
@@ -19,7 +19,6 @@ import com.liferay.frontend.taglib.form.navigator.constants.FormNavigatorConstan
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.util.Portal;
 
