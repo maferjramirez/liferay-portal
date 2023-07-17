5ad741e8549c300ca9f9d3d369b2e514b536f334
andrea.ale.sbarra <andrea.ale.sbarra@gmail.com>
2023-07-17 14:29:56 +0200

    COMMERCE-11740 sorted parameters

--- modules/apps/commerce/commerce-inventory-service/src/main/java/com/liferay/commerce/inventory/service/impl/CommerceInventoryAuditLocalServiceImpl.java
+++ modules/apps/commerce/commerce-inventory-service/src/main/java/com/liferay/commerce/inventory/service/impl/CommerceInventoryAuditLocalServiceImpl.java
@@ -40,8 +40,7 @@ public class CommerceInventoryAuditLocalServiceImpl
 
 	@Override
 	public CommerceInventoryAudit addCommerceInventoryAudit(
-			long userId, String sku, String unitOfMeasureKey, String logType,
-			String logTypeSettings, int quantity)
+            long userId, String logType, String logTypeSettings, int quantity, String sku, String unitOfMeasureKey)
 		throws PortalException {
 
 		User user = _userLocalService.getUser(userId);
