d47c803e97b7e4e7f8aa4c38ed66f07e2879ed20
Kenji Heigel <kenji.heigel@liferay.com>
2023-07-19 10:48:44 -0700

    Revert "POSHI-561 Change order to better follow arg orders"
    This reverts commit 83202fbcd64290245a1e0b5cff6a99395893401c.

--- modules/test/poshi/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/BaseWebDriverImpl.java
+++ modules/test/poshi/poshi-runner/src/main/java/com/liferay/poshi/runner/selenium/BaseWebDriverImpl.java
@@ -253,7 +253,7 @@ public abstract class BaseWebDriverImpl implements LiferaySelenium, WebDriver {
 	}
 
 	@Override
-	public void assertAttributeNotPresent(String locator, String attribute)
+	public void assertAttributeNotPresent(String attribute, String locator)
 		throws Exception {
 
 		if (isAttributePresent(attribute, locator)) {
@@ -263,7 +263,7 @@ public abstract class BaseWebDriverImpl implements LiferaySelenium, WebDriver {
 	}
 
 	@Override
-	public void assertAttributePresent(String locator, String attribute)
+	public void assertAttributePresent(String attribute, String locator)
 		throws Exception {
 
 		if (!isAttributePresent(attribute, locator)) {
@@ -274,7 +274,7 @@ public abstract class BaseWebDriverImpl implements LiferaySelenium, WebDriver {
 
 	@Override
 	public void assertAttributeValue(
-			String locator, String attribute, String pattern)
+			String attribute, String locator, String pattern)
 		throws Exception {
 
 		WebElement webElement = getWebElement(locator);
@@ -1560,12 +1560,12 @@ public abstract class BaseWebDriverImpl implements LiferaySelenium, WebDriver {
 	}
 
 	@Override
-	public boolean isAttributeNotPresent(String locator, String attribute) {
+	public boolean isAttributeNotPresent(String attribute, String locator) {
 		return !isAttributePresent(attribute, locator);
 	}
 
 	@Override
-	public boolean isAttributePresent(String locator, String attribute) {
+	public boolean isAttributePresent(String attribute, String locator) {
 		WebElement webElement = getWebElement(locator);
 
 		JavascriptExecutor javascriptExecutor =
