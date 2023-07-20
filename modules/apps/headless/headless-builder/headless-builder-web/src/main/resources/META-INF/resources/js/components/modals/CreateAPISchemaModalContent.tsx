17d5fc7be8ae9882894865792bb865c1054c33e3
Carlos Montenegro <carlos.montenegro@liferay.com>
2023-07-20 19:59:40 -0300

    LPS-190624 Create modal content for schema creation

---
+++ modules/apps/headless/headless-builder/headless-builder-web/src/main/resources/META-INF/resources/js/components/modals/CreateAPISchemaModalContent.tsx
@@ -0,0 +1,193 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayButton from '@clayui/button';
+import ClayModal from '@clayui/modal';
+import {fetch, openToast} from 'frontend-js-web';
+import React, {useEffect, useState} from 'react';
+
+import BaseAPISchemaFields from '../baseComponents/BaseAPISchemaFields';
+
+type DataError = {
+	description: boolean;
+	mainObjectDefinitionERC: boolean;
+	name: boolean;
+};
+
+interface CreateAPISchemaModalProps {
+	apiSchemasURLPath: string;
+	closeModal: voidReturn;
+	currentAPIApplicationID: string;
+	loadData: voidReturn;
+}
+
+const headers = new Headers({
+	'Accept': 'application/json',
+	'Accept-Language': Liferay.ThemeDisplay.getBCP47LanguageId(),
+	'Content-Type': 'application/json',
+});
+
+export function CreateAPISchemaModalContent({
+	apiSchemasURLPath,
+	closeModal,
+	currentAPIApplicationID,
+	loadData,
+}: CreateAPISchemaModalProps) {
+	const [data, setData] = useState<Partial<APIApplicationSchemaItem>>({
+		r_apiApplicationToAPISchemas_c_apiApplicationId: currentAPIApplicationID,
+	});
+	const [displayError, setDisplayError] = useState<DataError>({
+		description: false,
+		mainObjectDefinitionERC: false,
+		name: false,
+	});
+
+	useEffect(() => {
+		for (const key in data) {
+			if (data[key as keyof APIApplicationSchemaItem] !== '') {
+				setDisplayError((previousErrors) => ({
+					...previousErrors,
+					[key]: false,
+				}));
+			}
+		}
+	}, [data]);
+
+	async function postData() {
+		fetch(apiSchemasURLPath, {
+			body: JSON.stringify({
+				...data,
+				applicationStatus: {key: 'unpublished'},
+				version: '1.0',
+			}),
+			headers,
+			method: 'POST',
+		})
+			.then((response) => {
+				if (response.ok) {
+					closeModal();
+					loadData();
+					openToast({
+						message: Liferay.Language.get(
+							'new-api-application-schema-was-created'
+						),
+						type: 'success',
+					});
+				}
+				else {
+					return response.json();
+				}
+			})
+			.then((responseJson) => {
+				if (responseJson) {
+					throw new Error(responseJson.title);
+				}
+			})
+			.catch((error) => {
+				openToast({
+					message: error.message,
+					type: 'danger',
+				});
+			});
+	}
+
+	function validateData() {
+		let isDataValid = true;
+		const mandatoryFields = [
+			'description',
+			'mainObjectDefinitionERC',
+			'name',
+		];
+
+		if (!Object.keys(data).length) {
+			const errors = mandatoryFields.reduce(
+				(errors, field) => ({...errors, [field]: true}),
+				{}
+			);
+			setDisplayError(errors as DataError);
+
+			isDataValid = false;
+		}
+		else {
+			mandatoryFields.forEach((field) => {
+				if (data[field as keyof APIApplicationSchemaItem]) {
+					setDisplayError((previousErrors) => ({
+						...previousErrors,
+						[field]: false,
+					}));
+				}
+				else {
+					setDisplayError((previousErrors) => ({
+						...previousErrors,
+						[field]: true,
+					}));
+					isDataValid = false;
+				}
+			});
+		}
+
+		return isDataValid;
+	}
+
+	const handleCreate = () => {
+		const isDataValid = validateData();
+
+		if (isDataValid) {
+			postData();
+		}
+		else {
+			return;
+		}
+	};
+
+	return (
+		<>
+			<ClayModal.Header>
+				{Liferay.Language.get('new-schema')}
+			</ClayModal.Header>
+
+			<div className="modal-body">
+				<BaseAPISchemaFields
+					data={data}
+					displayError={displayError}
+					setData={setData}
+				/>
+			</div>
+
+			<ClayModal.Footer
+				last={
+					<ClayButton.Group spaced>
+						<ClayButton
+							displayType="secondary"
+							id="modalCancelButton"
+							onClick={closeModal}
+							type="button"
+						>
+							{Liferay.Language.get('cancel')}
+						</ClayButton>
+
+						<ClayButton
+							displayType="primary"
+							id="modalCreateButton"
+							onClick={handleCreate}
+							type="button"
+						>
+							{Liferay.Language.get('create')}
+						</ClayButton>
+					</ClayButton.Group>
+				}
+			/>
+		</>
+	);
+}
