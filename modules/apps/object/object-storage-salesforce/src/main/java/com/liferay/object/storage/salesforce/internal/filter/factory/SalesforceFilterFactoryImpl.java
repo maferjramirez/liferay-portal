2a9ab53695d37fecab59246ec8aa1e57434efda1
p-albuquerque <paulo.albuquerque@liferay.com>
2023-07-21 14:21:27 -0300

    LPS-190738 SF

--- modules/apps/object/object-storage-salesforce/src/main/java/com/liferay/object/storage/salesforce/internal/filter/factory/SalesforceFilterFactoryImpl.java
+++ modules/apps/object/object-storage-salesforce/src/main/java/com/liferay/object/storage/salesforce/internal/filter/factory/SalesforceFilterFactoryImpl.java
@@ -25,10 +25,11 @@ import com.liferay.portal.odata.entity.EntityModel;
 import com.liferay.portal.odata.filter.InvalidFilterException;
 import com.liferay.portal.odata.filter.expression.Expression;
 import com.liferay.portal.odata.filter.expression.ExpressionVisitException;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 import javax.ws.rs.ServerErrorException;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Paulo Albuquerque
