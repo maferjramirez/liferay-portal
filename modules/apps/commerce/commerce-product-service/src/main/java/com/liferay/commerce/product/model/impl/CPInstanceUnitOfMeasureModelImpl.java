782de156d100fd00e59497169ce524a03b19069d
Alberto Chaparro <alberto.chaparro@liferay.com>
2023-07-23 19:21:27 +0200

    LPS-190855 ServiceBuilder

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPInstanceUnitOfMeasureModelImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/model/impl/CPInstanceUnitOfMeasureModelImpl.java
@@ -112,7 +112,7 @@ public class CPInstanceUnitOfMeasureModelImpl
 	}
 
 	public static final String TABLE_SQL_CREATE =
-		"create table CPInstanceUOM (mvccVersion LONG default 0 not null,ctCollectionId LONG default 0 not null,uuid_ VARCHAR(75) null,CPInstanceUOMId LONG not null,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,CPInstanceId LONG,active_ BOOLEAN,incrementalOrderQuantity DECIMAL(30, 16) null,key_ VARCHAR(75) null,name STRING null,precision_ INTEGER,primary_ BOOLEAN,priority DOUBLE,rate DECIMAL(30, 16) null,sku VARCHAR(75) null,primary key (CPInstanceUOMId, ctCollectionId))";
+		"create table CPInstanceUOM (mvccVersion LONG default 0 not null,ctCollectionId LONG default 0 not null,uuid_ VARCHAR(75) null,CPInstanceUOMId LONG not null,companyId LONG,userId LONG,userName VARCHAR(75) null,createDate DATE null,modifiedDate DATE null,CPInstanceId LONG,active_ BOOLEAN,incrementalOrderQuantity BIGDECIMAL null,key_ VARCHAR(75) null,name STRING null,precision_ INTEGER,primary_ BOOLEAN,priority DOUBLE,rate BIGDECIMAL null,sku VARCHAR(75) null,primary key (CPInstanceUOMId, ctCollectionId))";
 
 	public static final String TABLE_SQL_DROP = "drop table CPInstanceUOM";
 
