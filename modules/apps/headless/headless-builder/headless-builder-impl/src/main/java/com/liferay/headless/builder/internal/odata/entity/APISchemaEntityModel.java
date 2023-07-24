a8a5be88789be08db758fbd8caa64cf9ffbfe03c
Carlos Correa <carlos.correa@liferay.com>
2023-07-24 16:43:01 +0200

    LPS-191058 Implement the EntityField, EntitySchema and ExpressionVisitor as we need to translate the fields that the user knows (the fields from the APISchema) to the internal fields that the Object's framework know (the fields from the Object Definition)

---
+++ modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/odata/entity/APISchemaEntityModel.java
@@ -0,0 +1,96 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.builder.internal.odata.entity;
+
+import com.liferay.headless.builder.application.APIApplication;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.odata.entity.CollectionEntityField;
+import com.liferay.portal.odata.entity.EntityField;
+import com.liferay.portal.odata.entity.EntityModel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author Carlos Correa
+ */
+public class APISchemaEntityModel implements EntityModel {
+
+	public APISchemaEntityModel(
+		EntityModel entityModel, APIApplication.Schema schema) {
+
+		_entityModel = entityModel;
+		_schema = schema;
+	}
+
+	@Override
+	public Map<String, EntityField> getEntityFieldsMap() {
+		Map<String, EntityField> entityModelEntityFieldsMap =
+			_entityModel.getEntityFieldsMap();
+
+		if (MapUtil.isEmpty(entityModelEntityFieldsMap)) {
+			return entityModelEntityFieldsMap;
+		}
+
+		Map<String, EntityField> entityModelFieldsMap = new HashMap<>();
+
+		for (Map.Entry<String, EntityField> entry :
+				entityModelEntityFieldsMap.entrySet()) {
+
+			for (APIApplication.Property property : _schema.getProperties()) {
+				if (StringUtil.equals(
+						property.getSourceFieldName(), entry.getKey())) {
+
+					EntityField entityField = entry.getValue();
+
+					entityModelFieldsMap.put(
+						property.getName(),
+						_toAPIPropertyEntityField(entityField, property));
+
+					break;
+				}
+			}
+		}
+
+		return entityModelFieldsMap;
+	}
+
+	@Override
+	public Map<String, EntityRelationship> getEntityRelationshipsMap() {
+		return _entityModel.getEntityRelationshipsMap();
+	}
+
+	@Override
+	public String getName() {
+		return _entityModel.getName();
+	}
+
+	private EntityField _toAPIPropertyEntityField(
+		EntityField entityField, APIApplication.Property property) {
+
+		if (Objects.equals(
+				entityField.getType(), EntityField.Type.COLLECTION)) {
+
+			CollectionEntityField collectionEntityField =
+				(CollectionEntityField)entityField;
+
+			return new CollectionEntityField(
+				_toAPIPropertyEntityField(
+					collectionEntityField.getEntityField(), property));
+		}
+
+		return new APIPropertyEntityField(
+			entityField.getName(), property.getName(), entityField.getType(),
+			locale -> property.getName(), locale -> property.getName(),
+			locale -> property.getName());
+	}
+
+	private final EntityModel _entityModel;
+	private final APIApplication.Schema _schema;
+
+}
