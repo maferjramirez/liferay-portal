570f3896eac1795a1c7f8b98a523f3972759f6b8
Jürgen Kappler <jurgen.kappler@liferay.com>
2023-07-24 17:14:10 -0600

    LPS-191297 Adds integrations tests

--- modules/apps/asset/asset-categories-test/src/testIntegration/java/com/liferay/asset/categories/service/test/AssetCategoryLocalServiceTest.java
+++ modules/apps/asset/asset-categories-test/src/testIntegration/java/com/liferay/asset/categories/service/test/AssetCategoryLocalServiceTest.java
@@ -7,6 +7,8 @@ package com.liferay.asset.categories.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.categories.configuration.AssetCategoriesCompanyConfiguration;
+import com.liferay.asset.category.property.model.AssetCategoryProperty;
+import com.liferay.asset.category.property.service.AssetCategoryPropertyLocalService;
 import com.liferay.asset.kernel.exception.AssetCategoryLimitException;
 import com.liferay.asset.kernel.exception.AssetCategoryNameException;
 import com.liferay.asset.kernel.model.AssetCategory;
@@ -14,25 +16,38 @@ import com.liferay.asset.kernel.model.AssetCategoryConstants;
 import com.liferay.asset.kernel.model.AssetVocabulary;
 import com.liferay.asset.kernel.service.AssetCategoryLocalService;
 import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
+import com.liferay.journal.constants.JournalFolderConstants;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.test.context.ContextUserReplace;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -53,7 +68,9 @@ public class AssetCategoryLocalServiceTest {
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new LiferayIntegrationTestRule();
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
 
 	@Before
 	public void setUp() throws Exception {
@@ -191,6 +208,317 @@ public class AssetCategoryLocalServiceTest {
 	}
 
 	@Test
+	public void testGetCategories() throws Exception {
+		JournalArticle journalArticle = _addJournalArticleWithAssetCategories(
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId()));
+
+		List<AssetCategory> assetCategories =
+			_assetCategoryLocalService.getCategories(
+				_portal.getClassNameId(JournalArticle.class.getName()),
+				journalArticle.getResourcePrimKey(), QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS);
+
+		Assert.assertNotNull(assetCategories);
+
+		Assert.assertEquals(
+			assetCategories.toString(), 2, assetCategories.size());
+	}
+
+	@Test
+	public void testGetCategoriesCount() throws Exception {
+		JournalArticle journalArticle = _addJournalArticleWithAssetCategories(
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId()));
+
+		Assert.assertEquals(
+			2,
+			_assetCategoryLocalService.getCategoriesCount(
+				_portal.getClassNameId(JournalArticle.class.getName()),
+				journalArticle.getResourcePrimKey()));
+	}
+
+	@Test
+	public void testGetCategoriesCountWithoutPermissions() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		serviceContext.setAddGroupPermissions(false);
+		serviceContext.setAddGuestPermissions(false);
+
+		JournalArticle journalArticle = _addJournalArticleWithAssetCategories(
+			serviceContext);
+
+		User user = UserTestUtil.addUser();
+
+		PermissionChecker permissionChecker =
+			PermissionCheckerFactoryUtil.create(user);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				user, permissionChecker)) {
+
+			Assert.assertEquals(
+				0,
+				_assetCategoryLocalService.getCategoriesCount(
+					_portal.getClassNameId(JournalArticle.class.getName()),
+					journalArticle.getResourcePrimKey()));
+		}
+		finally {
+			UserLocalServiceUtil.deleteUser(user);
+		}
+	}
+
+	@Test
+	public void testGetCategoriesWithoutPermissions() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		serviceContext.setAddGroupPermissions(false);
+		serviceContext.setAddGuestPermissions(false);
+
+		JournalArticle journalArticle = _addJournalArticleWithAssetCategories(
+			serviceContext);
+
+		User user = UserTestUtil.addUser();
+
+		PermissionChecker permissionChecker =
+			PermissionCheckerFactoryUtil.create(user);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				user, permissionChecker)) {
+
+			List<AssetCategory> assetCategories =
+				_assetCategoryLocalService.getCategories(
+					_portal.getClassNameId(JournalArticle.class.getName()),
+					journalArticle.getResourcePrimKey(), QueryUtil.ALL_POS,
+					QueryUtil.ALL_POS);
+
+			Assert.assertTrue(ListUtil.isEmpty(assetCategories));
+		}
+		finally {
+			UserLocalServiceUtil.deleteUser(user);
+		}
+	}
+
+	@Test
+	public void testSearch() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		AssetCategory assetCategory1 = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), _assetVocabulary.getVocabularyId(),
+			serviceContext);
+		AssetCategory assetCategory2 = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), _assetVocabulary.getVocabularyId(),
+			serviceContext);
+
+		List<AssetCategory> assetCategories = _assetCategoryLocalService.search(
+			_group.getGroupId(), null, null, QueryUtil.ALL_POS,
+			QueryUtil.ALL_POS);
+
+		Assert.assertNotNull(assetCategories);
+
+		Assert.assertEquals(
+			assetCategories.toString(), 2, assetCategories.size());
+
+		Assert.assertTrue(assetCategories.contains(assetCategory1));
+		Assert.assertTrue(assetCategories.contains(assetCategory2));
+	}
+
+	@Test
+	public void testSearchWithAssetCategoryProperty() throws Exception {
+		AssetCategory assetCategory1 = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), _assetVocabulary.getVocabularyId(),
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId()));
+
+		AssetCategoryProperty assetCategoryProperty1 =
+			_assetCategoryPropertyLocalService.addCategoryProperty(
+				TestPropsValues.getUserId(), assetCategory1.getCategoryId(),
+				"key1", "value1");
+
+		_assetCategoryPropertyLocalService.addCategoryProperty(
+			TestPropsValues.getUserId(), assetCategory1.getCategoryId(), "key2",
+			"value2");
+
+		String[] assetCategoryProperties = {
+			StringBundler.concat(
+				assetCategoryProperty1.getKey(),
+				AssetCategoryConstants.PROPERTY_KEY_VALUE_SEPARATOR,
+				assetCategoryProperty1.getValue())
+		};
+
+		List<AssetCategory> assetCategories = _assetCategoryLocalService.search(
+			_group.getGroupId(), null, assetCategoryProperties,
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		Assert.assertNotNull(assetCategories);
+
+		Assert.assertEquals(
+			assetCategories.toString(), 1, assetCategories.size());
+
+		Assert.assertTrue(assetCategories.contains(assetCategory1));
+	}
+
+	@Test
+	public void testSearchWithName() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		AssetCategory assetCategory1 = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), _assetVocabulary.getVocabularyId(),
+			serviceContext);
+
+		_assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), _assetVocabulary.getVocabularyId(),
+			serviceContext);
+
+		List<AssetCategory> assetCategories = _assetCategoryLocalService.search(
+			_group.getGroupId(), assetCategory1.getName(), null,
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		Assert.assertNotNull(assetCategories);
+
+		Assert.assertEquals(
+			assetCategories.toString(), 1, assetCategories.size());
+
+		Assert.assertTrue(assetCategories.contains(assetCategory1));
+	}
+
+	@Test
+	public void testSearchWithNameAndMultipleAssetCategoryProperties()
+		throws Exception {
+
+		AssetCategory assetCategory = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), _assetVocabulary.getVocabularyId(),
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId()));
+
+		AssetCategoryProperty assetCategoryProperty1 =
+			_assetCategoryPropertyLocalService.addCategoryProperty(
+				TestPropsValues.getUserId(), assetCategory.getCategoryId(),
+				"key1", "value1");
+		AssetCategoryProperty assetCategoryProperty2 =
+			_assetCategoryPropertyLocalService.addCategoryProperty(
+				TestPropsValues.getUserId(), assetCategory.getCategoryId(),
+				"key2", "value2");
+
+		String[] assetCategoryProperties = {
+			StringBundler.concat(
+				assetCategoryProperty1.getKey(),
+				AssetCategoryConstants.PROPERTY_KEY_VALUE_SEPARATOR,
+				assetCategoryProperty1.getValue()),
+			StringBundler.concat(
+				assetCategoryProperty2.getKey(),
+				AssetCategoryConstants.PROPERTY_KEY_VALUE_SEPARATOR,
+				assetCategoryProperty2.getValue())
+		};
+
+		List<AssetCategory> assetCategories = _assetCategoryLocalService.search(
+			_group.getGroupId(), assetCategory.getName(),
+			assetCategoryProperties, QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		Assert.assertNotNull(assetCategories);
+
+		Assert.assertEquals(
+			assetCategories.toString(), 1, assetCategories.size());
+
+		Assert.assertTrue(assetCategories.contains(assetCategory));
+	}
+
+	@Test
+	public void testSearchWithSameAssetCategoryPropertiesInDifferentVocabularies()
+		throws Exception {
+
+		AssetCategory assetCategory1 = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), _assetVocabulary.getVocabularyId(),
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId()));
+
+		_assetCategoryPropertyLocalService.addCategoryProperty(
+			TestPropsValues.getUserId(), assetCategory1.getCategoryId(), "key1",
+			"value1");
+
+		AssetVocabulary assetVocabulary =
+			_assetVocabularyLocalService.addVocabulary(
+				TestPropsValues.getUserId(), _group.getGroupId(),
+				RandomTestUtil.randomString(), new ServiceContext());
+
+		AssetCategory assetCategory2 = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), assetVocabulary.getVocabularyId(),
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId()));
+
+		_assetCategoryPropertyLocalService.addCategoryProperty(
+			TestPropsValues.getUserId(), assetCategory2.getCategoryId(), "key1",
+			"value1");
+
+		String[] assetCategoryProperties = {
+			StringBundler.concat(
+				"key1", AssetCategoryConstants.PROPERTY_KEY_VALUE_SEPARATOR,
+				"value1")
+		};
+
+		List<AssetCategory> assetCategories = _assetCategoryLocalService.search(
+			_group.getGroupId(), null, assetCategoryProperties,
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		Assert.assertNotNull(assetCategories);
+
+		Assert.assertEquals(
+			assetCategories.toString(), 2, assetCategories.size());
+
+		Assert.assertTrue(assetCategories.contains(assetCategory1));
+		Assert.assertTrue(assetCategories.contains(assetCategory2));
+	}
+
+	@Test
+	public void testSearchWithSameNameInDifferentVocabulary() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		String assetCategoryName = RandomTestUtil.randomString();
+
+		AssetCategory assetCategory1 = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(), assetCategoryName,
+			_assetVocabulary.getVocabularyId(), serviceContext);
+
+		AssetVocabulary assetVocabulary =
+			_assetVocabularyLocalService.addVocabulary(
+				TestPropsValues.getUserId(), _group.getGroupId(),
+				RandomTestUtil.randomString(), serviceContext);
+
+		AssetCategory assetCategory2 = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(), assetCategoryName,
+			assetVocabulary.getVocabularyId(), serviceContext);
+
+		List<AssetCategory> assetCategories = _assetCategoryLocalService.search(
+			_group.getGroupId(), assetCategoryName, null, QueryUtil.ALL_POS,
+			QueryUtil.ALL_POS);
+
+		Assert.assertNotNull(assetCategories);
+
+		Assert.assertEquals(
+			assetCategories.toString(), 2, assetCategories.size());
+
+		Assert.assertTrue(assetCategories.contains(assetCategory1));
+		Assert.assertTrue(assetCategories.contains(assetCategory2));
+	}
+
+	@Test
 	public void testUpdateAssetCategoryWithMissingTranslationInSiteDefaultLocale()
 		throws Exception {
 
@@ -261,8 +589,35 @@ public class AssetCategoryLocalServiceTest {
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
 
+	private JournalArticle _addJournalArticleWithAssetCategories(
+			ServiceContext serviceContext)
+		throws Exception {
+
+		AssetCategory assetCategory1 = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), _assetVocabulary.getVocabularyId(),
+			serviceContext);
+		AssetCategory assetCategory2 = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), _assetVocabulary.getVocabularyId(),
+			serviceContext);
+
+		serviceContext.setAssetCategoryIds(
+			new long[] {
+				assetCategory1.getCategoryId(), assetCategory2.getCategoryId()
+			});
+
+		return JournalTestUtil.addArticle(
+			_group.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID, serviceContext);
+	}
+
 	@Inject
 	private AssetCategoryLocalService _assetCategoryLocalService;
+
+	@Inject
+	private AssetCategoryPropertyLocalService
+		_assetCategoryPropertyLocalService;
 
 	@DeleteAfterTestRun
 	private AssetVocabulary _assetVocabulary;
@@ -275,5 +630,8 @@ public class AssetCategoryLocalServiceTest {
 
 	@DeleteAfterTestRun
 	private Group _group;
+
+	@Inject
+	private Portal _portal;
 
 }
