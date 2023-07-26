77e0ca3e886f7b90f744176a24e5a48eb64da8dd
Sandro Chinea <sandrochinea@gmail.com>
2023-07-26 11:46:38 +0100

    LPS-191764 Open a toast when adding a composition

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/thunks/addFragmentComposition.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/thunks/addFragmentComposition.js
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {openToast, sub} from 'frontend-js-web';
+
 import addFragmentCompositionAction from '../actions/addFragmentComposition';
 import FragmentService from '../services/FragmentService';
 
@@ -26,13 +28,23 @@ export default function addFragmentComposition({
 			saveInlineContent,
 			saveMappingConfiguration,
 			segmentsExperienceId: getState().segmentsExperienceId,
-		}).then((fragmentComposition) => {
+		}).then(({fragmentComposition, url}) => {
 			dispatch(
 				addFragmentCompositionAction({
 					fragmentCollectionId,
 					fragmentComposition,
 				})
 			);
+
+			openToast({
+				message: sub(
+					Liferay.Language.get(
+						'the-fragment-was-created-successfully.-you-can-view-it-in-x'
+					),
+					`<a href="${url}">${Liferay.Language.get('fragments')}</a>`
+				),
+				type: 'success',
+			});
 		});
 	};
 }
