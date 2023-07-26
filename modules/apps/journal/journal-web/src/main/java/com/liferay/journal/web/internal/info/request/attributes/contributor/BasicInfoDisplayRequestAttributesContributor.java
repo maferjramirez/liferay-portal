b7e80261bb44c5781cd7c80cb60738c1b08adbad
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-07-26 14:43:10 +0100

    LPS-191813 Since it is only for journal, update logic accordingly

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/request/attributes/contributor/BasicInfoDisplayRequestAttributesContributor.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/request/attributes/contributor/BasicInfoDisplayRequestAttributesContributor.java
@@ -1,15 +1,15 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.journal.web.internal.info.request.attributes.contributor;
 
-import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
-import com.liferay.asset.kernel.model.AssetRendererFactory;
 import com.liferay.info.constants.InfoDisplayWebKeys;
 import com.liferay.info.display.request.attributes.contributor.InfoDisplayRequestAttributesContributor;
 import com.liferay.info.item.InfoItemDetails;
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.journal.model.JournalArticle;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Portlet;
@@ -24,6 +24,8 @@ import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portlet.RenderRequestFactory;
 import com.liferay.portlet.RenderResponseFactory;
+
+import java.util.Objects;
 
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletMode;
@@ -44,64 +46,59 @@ public class BasicInfoDisplayRequestAttributesContributor
 
 	@Override
 	public void addAttributes(HttpServletRequest httpServletRequest) {
-		AssetRendererFactory<?> assetRendererFactory = _getAssetRendererFactory(
-			httpServletRequest);
+		InfoItemDetails infoItemDetails =
+			(InfoItemDetails)httpServletRequest.getAttribute(
+				InfoDisplayWebKeys.INFO_ITEM_DETAILS);
+
+		if ((infoItemDetails == null) ||
+			!Objects.equals(
+				JournalArticle.class.getName(),
+				infoItemDetails.getClassName())) {
+
+			return;
+		}
 
 		PortletRequest portletRequest =
 			(PortletRequest)httpServletRequest.getAttribute(
 				JavaConstants.JAVAX_PORTLET_REQUEST);
 
-		if ((portletRequest == null) && (assetRendererFactory != null)) {
-			Portlet portlet = _portletLocalService.getPortletById(
-				assetRendererFactory.getPortletId());
-
-			ThemeDisplay themeDisplay =
-				(ThemeDisplay)httpServletRequest.getAttribute(
-					WebKeys.THEME_DISPLAY);
-
-			try {
-				InvokerPortlet invokerPortlet =
-					PortletInstanceFactoryUtil.create(
-						portlet, httpServletRequest.getServletContext());
-
-				PortletConfig portletConfig = PortletConfigFactoryUtil.create(
-					portlet, httpServletRequest.getServletContext());
-
-				LiferayRenderRequest liferayRenderRequest =
-					RenderRequestFactory.create(
-						httpServletRequest, portlet, invokerPortlet,
-						portletConfig.getPortletContext(), WindowState.NORMAL,
-						PortletMode.VIEW,
-						PortletPreferencesFactoryUtil.fromDefaultXML(
-							portlet.getDefaultPreferences()),
-						themeDisplay.getPlid());
-
-				httpServletRequest.setAttribute(
-					JavaConstants.JAVAX_PORTLET_REQUEST, liferayRenderRequest);
-				httpServletRequest.setAttribute(
-					JavaConstants.JAVAX_PORTLET_RESPONSE,
-					RenderResponseFactory.create(
-						themeDisplay.getResponse(), liferayRenderRequest));
-			}
-			catch (Exception exception) {
-				_log.error(exception);
-			}
-		}
-	}
-
-	private AssetRendererFactory<?> _getAssetRendererFactory(
-		HttpServletRequest httpServletRequest) {
-
-		InfoItemDetails infoItemDetails =
-			(InfoItemDetails)httpServletRequest.getAttribute(
-				InfoDisplayWebKeys.INFO_ITEM_DETAILS);
-
-		if (infoItemDetails == null) {
-			return null;
+		if (portletRequest != null) {
+			return;
 		}
 
-		return AssetRendererFactoryRegistryUtil.
-			getAssetRendererFactoryByClassName(infoItemDetails.getClassName());
+		Portlet portlet = _portletLocalService.getPortletById(
+			JournalPortletKeys.JOURNAL);
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		try {
+			InvokerPortlet invokerPortlet = PortletInstanceFactoryUtil.create(
+				portlet, httpServletRequest.getServletContext());
+
+			PortletConfig portletConfig = PortletConfigFactoryUtil.create(
+				portlet, httpServletRequest.getServletContext());
+
+			LiferayRenderRequest liferayRenderRequest =
+				RenderRequestFactory.create(
+					httpServletRequest, portlet, invokerPortlet,
+					portletConfig.getPortletContext(), WindowState.NORMAL,
+					PortletMode.VIEW,
+					PortletPreferencesFactoryUtil.fromDefaultXML(
+						portlet.getDefaultPreferences()),
+					themeDisplay.getPlid());
+
+			httpServletRequest.setAttribute(
+				JavaConstants.JAVAX_PORTLET_REQUEST, liferayRenderRequest);
+			httpServletRequest.setAttribute(
+				JavaConstants.JAVAX_PORTLET_RESPONSE,
+				RenderResponseFactory.create(
+					themeDisplay.getResponse(), liferayRenderRequest));
+		}
+		catch (Exception exception) {
+			_log.error(exception);
+		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
