82adcba54960ae28031a3ba023adc3eb116754fa
Pablo Molina <contact@pablomolina.me>
2023-07-27 12:34:29 +0200

    LPS-169595 Rename to App.scss to it is compiled

--- modules/apps/site-navigation/site-navigation-admin-web/src/main/resources/META-INF/resources/site_navigation_menu_editor/components/App.js
+++ modules/apps/site-navigation/site-navigation-admin-web/src/main/resources/META-INF/resources/site_navigation_menu_editor/components/App.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import './_App.scss';
+import './App.scss';
 
 import React, {useRef} from 'react';
 import {DndProvider} from 'react-dnd';
