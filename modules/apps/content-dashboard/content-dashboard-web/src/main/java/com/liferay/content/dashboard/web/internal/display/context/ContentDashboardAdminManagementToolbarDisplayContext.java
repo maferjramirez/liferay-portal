0d28af5ec4a8540f010ccb2e2a7407253397dd39
Victor Galan <victor.galan@liferay.com>
2023-07-27 08:33:31 +0000

    LPS-191901 Get list of all the company vocabularies

--- modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/display/context/ContentDashboardAdminManagementToolbarDisplayContext.java
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/display/context/ContentDashboardAdminManagementToolbarDisplayContext.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -547,8 +547,8 @@ public class ContentDashboardAdminManagementToolbarDisplayContext
 		).setParameter(
 			"vocabularyIds",
 			() -> ListUtil.toString(
-				_assetVocabularyLocalService.getGroupVocabularies(
-					themeDisplay.getCompanyGroupId()),
+				_assetVocabularyLocalService.getCompanyVocabularies(
+					themeDisplay.getCompanyId()),
 				AssetVocabulary.VOCABULARY_ID_ACCESSOR)
 		).buildString();
 	}
