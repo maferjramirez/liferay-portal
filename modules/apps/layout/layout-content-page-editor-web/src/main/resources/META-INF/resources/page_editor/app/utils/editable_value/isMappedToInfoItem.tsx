ce2a807548af1d905f1c7a7d75bac5f3d533ae67
Sandro Chinea <sandrochinea@gmail.com>
2023-07-28 11:11:42 +0100

    LPS-189470 Migrate isMappedToInfoItem to typescript

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/editable_value/isMappedToInfoItem.tsx
@@ -0,0 +1,40 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+interface Editable {
+	classNameId?: string;
+	classPK?: string;
+	externalReferenceCode?: string;
+	fieldId?: string;
+}
+
+interface BaseMapped {
+	classNameId: string;
+	fieldId: string;
+}
+
+interface MappedWithClassPK extends BaseMapped {
+	classPK: string;
+}
+
+interface MappedWithERC extends BaseMapped {
+	externalReferenceCode: string;
+}
+
+type MappedEditable = MappedWithClassPK | MappedWithERC;
+
+export default function isMappedToInfoItem(
+	editable: Editable
+): editable is MappedEditable {
+	if (typeof editable !== 'object') {
+		return false;
+	}
+
+	return (
+		'classNameId' in editable &&
+		'fieldId' in editable &&
+		('classPK' in editable || 'externalReferenceCode' in editable)
+	);
+}
