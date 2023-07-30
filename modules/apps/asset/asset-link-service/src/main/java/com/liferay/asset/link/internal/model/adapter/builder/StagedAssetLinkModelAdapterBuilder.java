a8698483b8898200a355b6875cb16fe0b3724135
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-07-30 13:15:33 +0100

    LPS-192105 Moves class

--- modules/apps/asset/asset-link-service/src/main/java/com/liferay/asset/link/internal/model/adapter/builder/StagedAssetLinkModelAdapterBuilder.java
+++ modules/apps/asset/asset-link-service/src/main/java/com/liferay/asset/link/internal/model/adapter/builder/StagedAssetLinkModelAdapterBuilder.java
@@ -1,14 +1,14 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.staging.internal.model.adapter.builder;
+package com.liferay.asset.link.internal.model.adapter.builder;
 
-import com.liferay.asset.kernel.model.AssetLink;
-import com.liferay.asset.kernel.model.adapter.StagedAssetLink;
+import com.liferay.asset.link.internal.model.adapter.iml.StagedAssetLinkImpl;
+import com.liferay.asset.link.model.AssetLink;
+import com.liferay.asset.link.model.adapter.StagedAssetLink;
 import com.liferay.portal.kernel.model.adapter.builder.ModelAdapterBuilder;
-import com.liferay.portlet.asset.model.adapter.impl.StagedAssetLinkImpl;
 
 import org.osgi.service.component.annotations.Component;
 
