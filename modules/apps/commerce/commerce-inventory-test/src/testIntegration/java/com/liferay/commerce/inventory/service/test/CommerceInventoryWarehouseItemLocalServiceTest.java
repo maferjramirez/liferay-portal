87067e29de020a842384f85f9ebbf46b4be4a751
andrea.ale.sbarra <andrea.ale.sbarra@gmail.com>
2023-07-31 14:44:30 +0200

    COMMERCE-11659 integration tests to check UOM for inventory

--- modules/apps/commerce/commerce-inventory-test/src/testIntegration/java/com/liferay/commerce/inventory/service/test/CommerceInventoryWarehouseItemLocalServiceTest.java
+++ modules/apps/commerce/commerce-inventory-test/src/testIntegration/java/com/liferay/commerce/inventory/service/test/CommerceInventoryWarehouseItemLocalServiceTest.java
@@ -1,43 +1,29 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.commerce.inventory.service.test;
 
-import com.liferay.account.constants.AccountConstants;
-import com.liferay.account.model.AccountEntry;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.commerce.account.test.util.CommerceAccountTestUtil;
-import com.liferay.commerce.context.CommerceContext;
-import com.liferay.commerce.currency.model.CommerceCurrency;
-import com.liferay.commerce.currency.test.util.CommerceCurrencyTestUtil;
-import com.liferay.commerce.inventory.engine.CommerceInventoryEngine;
-import com.liferay.commerce.inventory.exception.DuplicateCommerceInventoryWarehouseItemException;
 import com.liferay.commerce.inventory.model.CommerceInventoryWarehouse;
 import com.liferay.commerce.inventory.model.CommerceInventoryWarehouseItem;
-import com.liferay.commerce.inventory.service.CommerceInventoryBookedQuantityLocalService;
 import com.liferay.commerce.inventory.service.CommerceInventoryWarehouseItemLocalService;
 import com.liferay.commerce.inventory.service.CommerceInventoryWarehouseLocalService;
-import com.liferay.commerce.model.CommerceOrder;
-import com.liferay.commerce.product.constants.CommerceChannelConstants;
+import com.liferay.commerce.product.exception.CPInstanceUnitOfMeasureKeyException;
+import com.liferay.commerce.product.exception.NoSuchCPInstanceUnitOfMeasureException;
 import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.commerce.product.model.CPInstanceUnitOfMeasure;
-import com.liferay.commerce.product.model.CommerceChannel;
 import com.liferay.commerce.product.service.CPInstanceLocalService;
 import com.liferay.commerce.product.service.CPInstanceUnitOfMeasureLocalService;
-import com.liferay.commerce.product.service.CommerceChannelLocalService;
-import com.liferay.commerce.service.CommerceOrderItemLocalService;
-import com.liferay.commerce.service.CommerceOrderLocalService;
 import com.liferay.commerce.test.util.CommerceInventoryTestUtil;
-import com.liferay.commerce.test.util.context.TestCommerceContext;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
@@ -45,7 +31,13 @@ import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.math.BigDecimal;
+
+import java.util.List;
+
 import org.frutilla.FrutillaRule;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -54,107 +46,296 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author Andrea Sbarra
  */
 @RunWith(Arquillian.class)
 public class CommerceInventoryWarehouseItemLocalServiceTest {
 
-    @ClassRule
-    @Rule
-    public static final AggregateTestRule aggregateTestRule =
-            new AggregateTestRule(
-                    new LiferayIntegrationTestRule(),
-                    PermissionCheckerMethodTestRule.INSTANCE);
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
 
-    @Before
-    public void setUp() throws Exception {
-        _group = GroupTestUtil.addGroup();
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
 
-        _user = UserTestUtil.addUser();
+		_user = UserTestUtil.addUser();
 
-        _serviceContext = ServiceContextTestUtil.getServiceContext(
-                _group.getCompanyId(), _group.getGroupId(), _user.getUserId());
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getCompanyId(), _group.getGroupId(), _user.getUserId());
 
-        _cpInstance = CommerceInventoryTestUtil.addRandomCPInstanceSku(
-                _group.getGroupId());
+		_cpInstance = CommerceInventoryTestUtil.addRandomCPInstanceSku(
+			_group.getGroupId());
 
-        _cpInstanceUnitOfMeasure =
-            _cpInstanceUnitOfMeasureLocalService.addCPInstanceUnitOfMeasure(
-                _user.getUserId(), _cpInstance.getCPInstanceId(), true, null,
-                "KEY",
-                HashMapBuilder.put(
-                        LocaleUtil.getDefault(), "NOME"
-                ).build(),
-                2, true, 0.0, BigDecimal.ONE, _cpInstance.getSku());
+		_cpInstanceUnitOfMeasure =
+			_cpInstanceUnitOfMeasureLocalService.addCPInstanceUnitOfMeasure(
+				_user.getUserId(), _cpInstance.getCPInstanceId(), true,
+				BigDecimal.TEN, "KEY",
+				HashMapBuilder.put(
+					LocaleUtil.getDefault(), "NOME"
+				).build(),
+				2, true, 0.0, BigDecimal.ONE, _cpInstance.getSku());
+	}
 
-    }
+	@After
+	public void tearDown() throws Exception {
+		List<CommerceInventoryWarehouse> commerceInventoryWarehouses =
+			_commerceInventoryWarehouseLocalService.
+				getCommerceInventoryWarehouses(_group.getCompanyId());
 
-    @After
-    public void tearDown() throws Exception {
-        List<CommerceInventoryWarehouse> commerceInventoryWarehouses =
-            _commerceInventoryWarehouseLocalService.
-                getCommerceInventoryWarehouses(_group.getCompanyId());
+		for (CommerceInventoryWarehouse commerceInventoryWarehouse :
+				commerceInventoryWarehouses) {
 
-        for (CommerceInventoryWarehouse commerceInventoryWarehouse :
-                commerceInventoryWarehouses) {
+			_commerceInventoryWarehouseLocalService.
+				deleteCommerceInventoryWarehouse(commerceInventoryWarehouse);
+		}
+	}
 
-            _commerceInventoryWarehouseLocalService.
-                deleteCommerceInventoryWarehouse(commerceInventoryWarehouse);
-        }
-    }
+	@Test
+	public void testAddWarehouseItemWithoutUOM() throws Exception {
+		frutillaRule.scenario(
+			"It should be possible to add item without UOM if the SKU have " +
+				"only one UOM"
+		).given(
+			"1 active warehouse"
+		).when(
+			"The item is added without the UOM"
+		).and(
+			"the sku has only one UOM"
+		).then(
+			"The Item is created"
+		).and(
+			"has the UOM of the SKU"
+		);
 
-    public void testWarehouseItemsWithRightUOM() throws Exception {
-        frutillaRule.scenario(
-                "It should be possible to add items with UOM of the SKU"
-        ).given(
-                "1 active warehouse"
-        ).when(
-                "The item is add with UOM of the SKU"
-        ).then(
-                "The Item is correctly created"
-        );
+		CommerceInventoryWarehouse commerceInventoryWarehouseActive =
+			CommerceInventoryTestUtil.addCommerceInventoryWarehouse(
+				true, _serviceContext);
 
-        CommerceInventoryWarehouse commerceInventoryWarehouseActive =
-                CommerceInventoryTestUtil.addCommerceInventoryWarehouse(
-                        true, _serviceContext);
+		CommerceInventoryWarehouseItem commerceInventoryWarehouseItem =
+			_commerceInventoryWarehouseItemLocalService.
+				addCommerceInventoryWarehouseItem(
+					StringPool.BLANK, _user.getUserId(),
+					commerceInventoryWarehouseActive.
+						getCommerceInventoryWarehouseId(),
+					BigDecimal.ONE, _cpInstance.getSku(), StringPool.BLANK);
 
-        Assert.assertNotNull(_commerceInventoryWarehouseItemLocalService.
-            addCommerceInventoryWarehouseItem(
-                StringPool.BLANK, _user.getUserId(),
-                commerceInventoryWarehouseActive.
-                    getCommerceInventoryWarehouseId(),
-                BigDecimal.ONE, _cpInstance.getSku(),
-                    _cpInstanceUnitOfMeasure.getKey()));
-    }
+		Assert.assertNotNull(commerceInventoryWarehouseItem);
+		Assert.assertEquals(
+			"UOM", _cpInstanceUnitOfMeasure.getKey(),
+			commerceInventoryWarehouseItem.getUnitOfMeasureKey());
+	}
 
-    @Rule
-    public FrutillaRule frutillaRule = new FrutillaRule();
+	@Test(expected = CPInstanceUnitOfMeasureKeyException.class)
+	public void testAddWarehouseItemWithoutUOMFails() throws Exception {
+		frutillaRule.scenario(
+			"It should not be possible to add item without UOM if the SKU " +
+				"has more then one UOM"
+		).given(
+			"1 active warehouse"
+		).when(
+			"The item is added without the UOM"
+		).and(
+			"the SKU has more then one UOM"
+		).then(
+			"The Item is not created"
+		).and(
+			"CPInstanceUnitOfMeasureKeyException is thrown"
+		);
 
-    private static User _user;
+		CPInstance cpInstance =
+			CommerceInventoryTestUtil.addRandomCPInstanceSku(
+				_group.getGroupId());
 
-    @Inject
-    private CommerceInventoryWarehouseItemLocalService
-            _commerceInventoryWarehouseItemLocalService;
+		_cpInstanceUnitOfMeasureLocalService.addCPInstanceUnitOfMeasure(
+			_user.getUserId(), _cpInstance.getCPInstanceId(), true,
+			BigDecimal.TEN, "KEY1",
+			HashMapBuilder.put(
+				LocaleUtil.getDefault(), "NAME-1"
+			).build(),
+			2, true, 0.0, BigDecimal.ONE, cpInstance.getSku());
 
-    @Inject
-    private CommerceInventoryWarehouseLocalService
-            _commerceInventoryWarehouseLocalService;
+		_cpInstanceUnitOfMeasureLocalService.addCPInstanceUnitOfMeasure(
+			_user.getUserId(), _cpInstance.getCPInstanceId(), true,
+			BigDecimal.TEN, "KEY2",
+			HashMapBuilder.put(
+				LocaleUtil.getDefault(), "NAME-2"
+			).build(),
+			2, true, 0.0, BigDecimal.ONE, cpInstance.getSku());
 
-    private CPInstance _cpInstance;
+		CommerceInventoryWarehouse commerceInventoryWarehouseActive =
+			CommerceInventoryTestUtil.addCommerceInventoryWarehouse(
+				true, _serviceContext);
 
-    private CPInstanceUnitOfMeasure _cpInstanceUnitOfMeasure;
+		_commerceInventoryWarehouseItemLocalService.
+			addCommerceInventoryWarehouseItem(
+				StringPool.BLANK, _user.getUserId(),
+				commerceInventoryWarehouseActive.
+					getCommerceInventoryWarehouseId(),
+				BigDecimal.ONE, cpInstance.getSku(), StringPool.BLANK);
+	}
 
-    @Inject
-    private CPInstanceLocalService _cpInstanceLocalService;
+	@Test
+	public void testAddWarehouseItemWithRandomSKUAndUOM() throws Exception {
+		frutillaRule.scenario(
+			"It should be possible to add item with a UOM and the SKU does " +
+				"not belong the any catalog"
+		).given(
+			"1 active warehouse"
+		).when(
+			"The item is added with a UOM"
+		).then(
+			"The Item is correctly created"
+		);
 
-    @Inject
-    private CPInstanceUnitOfMeasureLocalService _cpInstanceUnitOfMeasureLocalService;
+		CommerceInventoryWarehouse commerceInventoryWarehouseActive =
+			CommerceInventoryTestUtil.addCommerceInventoryWarehouse(
+				true, _serviceContext);
 
-    private Group _group;
-    private ServiceContext _serviceContext;
+		Assert.assertNotNull(
+			_commerceInventoryWarehouseItemLocalService.
+				addCommerceInventoryWarehouseItem(
+					StringPool.BLANK, _user.getUserId(),
+					commerceInventoryWarehouseActive.
+						getCommerceInventoryWarehouseId(),
+					BigDecimal.ONE, RandomTestUtil.randomString(),
+					_cpInstanceUnitOfMeasure.getKey()));
+	}
+
+	@Test
+	public void testAddWarehouseItemWithRightUOM() throws Exception {
+		frutillaRule.scenario(
+			"It should be possible to add item with UOM of the SKU"
+		).given(
+			"1 active warehouse"
+		).when(
+			"The item is add with UOM of the SKU"
+		).then(
+			"The Item is correctly created"
+		);
+
+		CommerceInventoryWarehouse commerceInventoryWarehouseActive =
+			CommerceInventoryTestUtil.addCommerceInventoryWarehouse(
+				true, _serviceContext);
+
+		Assert.assertNotNull(
+			_commerceInventoryWarehouseItemLocalService.
+				addCommerceInventoryWarehouseItem(
+					StringPool.BLANK, _user.getUserId(),
+					commerceInventoryWarehouseActive.
+						getCommerceInventoryWarehouseId(),
+					BigDecimal.ONE, _cpInstance.getSku(),
+					_cpInstanceUnitOfMeasure.getKey()));
+	}
+
+	@Test(expected = NoSuchCPInstanceUnitOfMeasureException.class)
+	public void testAddWarehouseItemWithWrongUOM() throws Exception {
+		frutillaRule.scenario(
+			"It should not be possible to add an item with a UOM not present " +
+				"on the SKU"
+		).given(
+			"1 active warehouse"
+		).when(
+			"The item is add with a random UOM"
+		).then(
+			"The Item is not created"
+		).and(
+			"and a NoSuchCPInstanceUnitOfMeasureException is thrown"
+		);
+
+		CommerceInventoryWarehouse commerceInventoryWarehouseActive =
+			CommerceInventoryTestUtil.addCommerceInventoryWarehouse(
+				true, _serviceContext);
+
+		Assert.assertNotNull(
+			_commerceInventoryWarehouseItemLocalService.
+				addCommerceInventoryWarehouseItem(
+					StringPool.BLANK, _user.getUserId(),
+					commerceInventoryWarehouseActive.
+						getCommerceInventoryWarehouseId(),
+					BigDecimal.ONE, _cpInstance.getSku(),
+					RandomTestUtil.randomString()));
+	}
+
+	@Test(expected = NoSuchCPInstanceUnitOfMeasureException.class)
+	public void testAddWarehouseItemWithWrongUOMFails() throws Exception {
+		frutillaRule.scenario(
+			"It should not be possible to add items with a UOM if the SKU " +
+				"have more then one UOM and not the UOM used"
+		).given(
+			"1 active warehouse"
+		).when(
+			"The item is added with a random UOM"
+		).and(
+			"the SKU has more then one UOM"
+		).then(
+			"The Item is not created"
+		).and(
+			"NoSuchCPInstanceUnitOfMeasureException is thrown"
+		);
+
+		CPInstance cpInstance =
+			CommerceInventoryTestUtil.addRandomCPInstanceSku(
+				_group.getGroupId());
+
+		_cpInstanceUnitOfMeasureLocalService.addCPInstanceUnitOfMeasure(
+			_user.getUserId(), _cpInstance.getCPInstanceId(), true,
+			BigDecimal.TEN, "KEY1",
+			HashMapBuilder.put(
+				LocaleUtil.getDefault(), "NAME-1"
+			).build(),
+			2, true, 0.0, BigDecimal.ONE, cpInstance.getSku());
+
+		_cpInstanceUnitOfMeasureLocalService.addCPInstanceUnitOfMeasure(
+			_user.getUserId(), _cpInstance.getCPInstanceId(), true,
+			BigDecimal.TEN, "KEY2",
+			HashMapBuilder.put(
+				LocaleUtil.getDefault(), "NAME-2"
+			).build(),
+			2, true, 0.0, BigDecimal.ONE, cpInstance.getSku());
+
+		CommerceInventoryWarehouse commerceInventoryWarehouseActive =
+			CommerceInventoryTestUtil.addCommerceInventoryWarehouse(
+				true, _serviceContext);
+
+		_commerceInventoryWarehouseItemLocalService.
+			addCommerceInventoryWarehouseItem(
+				StringPool.BLANK, _user.getUserId(),
+				commerceInventoryWarehouseActive.
+					getCommerceInventoryWarehouseId(),
+				BigDecimal.ONE, cpInstance.getSku(),
+				RandomTestUtil.randomString());
+	}
+
+	@Rule
+	public FrutillaRule frutillaRule = new FrutillaRule();
+
+	private static User _user;
+
+	@Inject
+	private CommerceInventoryWarehouseItemLocalService
+		_commerceInventoryWarehouseItemLocalService;
+
+	@Inject
+	private CommerceInventoryWarehouseLocalService
+		_commerceInventoryWarehouseLocalService;
+
+	private CPInstance _cpInstance;
+
+	@Inject
+	private CPInstanceLocalService _cpInstanceLocalService;
+
+	private CPInstanceUnitOfMeasure _cpInstanceUnitOfMeasure;
+
+	@Inject
+	private CPInstanceUnitOfMeasureLocalService
+		_cpInstanceUnitOfMeasureLocalService;
+
+	private Group _group;
+	private ServiceContext _serviceContext;
+
 }
