0ec0e593389d472a5c7a413378b7c8427e1fc108
Victor Galan <victor.galan@liferay.com>
2023-08-01 11:51:30 +0200

    LPS-192140 if it is a list save it as a comma separated string

--- modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/request/struts/EditInfoItemStrutsAction.java
+++ modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/request/struts/EditInfoItemStrutsAction.java
@@ -63,6 +63,7 @@ import com.liferay.portal.kernel.struts.StrutsAction;
 import com.liferay.portal.kernel.upload.UploadServletRequest;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
@@ -425,7 +426,13 @@ public class EditInfoItemStrutsAction implements StrutsAction {
 			return simpleDateFormat.format(infoFieldValue.getValue());
 		}
 
-		return String.valueOf(infoFieldValue.getValue());
+		Object value = infoFieldValue.getValue();
+
+		if (value instanceof List) {
+			return ListUtil.toString((List<?>)value, StringPool.BLANK);
+		}
+
+		return String.valueOf(value);
 	}
 
 	private boolean _hasCaptcha(
