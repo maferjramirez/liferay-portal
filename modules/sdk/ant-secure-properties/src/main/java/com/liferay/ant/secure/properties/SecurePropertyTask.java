ae20fd43d43ea63255b4c9afe7b86acacfdcde96
Peter Yoo <peter.yoo@liferay.com>
2023-08-01 13:40:34 -0700

    LRCI-3673 Create empty SecurePropertyTask class

---
+++ modules/sdk/ant-secure-properties/src/main/java/com/liferay/ant/secure/properties/SecurePropertyTask.java
@@ -0,0 +1,11 @@
+package com.liferay.ant.secure.properties;
+
+import org.apache.tools.ant.taskdefs.Property;
+
+public class SecurePropertyTask extends Property {
+
+	@Override
+	protected void addProperty(String n, Object v) {
+	}
+	
+}
