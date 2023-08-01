2404b128c76689c162ed78982f24ee24b4819b46
Ilza Medeiros <ilza.medeiros@liferay.com>
2023-08-01 16:45:35 -0300

    LPS-192338 Add description on accessibility menu component

--- modules/apps/accessibility/accessibility-menu-web/src/main/resources/META-INF/resources/js/AccessibilitySetting.tsx
+++ modules/apps/accessibility/accessibility-menu-web/src/main/resources/META-INF/resources/js/AccessibilitySetting.tsx
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {Text} from '@clayui/core';
 import {ClayToggle} from '@clayui/form';
 import React from 'react';
 
@@ -10,7 +11,9 @@ const KEY_EVENT = 'Enter';
 
 type Props = {
 	className: string;
+	description: string;
 	disabled: boolean;
+	index: number;
 	label: string;
 	onChange: (value: boolean) => void;
 	value: boolean;
@@ -18,13 +21,16 @@ type Props = {
 
 const AccessibilitySetting = ({
 	className,
+	description,
 	disabled,
+	index,
 	label,
 	onChange,
 	value,
 }: Props) => (
 	<li className={className}>
 		<ClayToggle
+			aria-describedby={`accessibility-help-${index}`}
 			disabled={disabled}
 			label={label}
 			onKeyDown={(event) => {
@@ -35,6 +41,12 @@ const AccessibilitySetting = ({
 			onToggle={onChange}
 			toggled={value}
 		/>
+
+		<span className="toggle-switch-text">
+			<Text color="secondary" id={`accessibility-help-${index}`} size={3}>
+				{description}
+			</Text>
+		</span>
 	</li>
 );
 
