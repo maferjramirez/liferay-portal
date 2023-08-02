a9cec7e8ae8ec710d0dc5775687168e30d4dd4af
Carlos Correa <carlos.correa@liferay.com>
2023-08-02 11:27:58 +0200

    LPS-191058 Support ComplexEntityField for the related elements of the main object definition that is assigned to an API Schema

--- modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/odata/entity/APISchemaEntityModel.java
+++ modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/odata/entity/APISchemaEntityModel.java
@@ -6,9 +6,11 @@
 package com.liferay.headless.builder.internal.odata.entity;
 
 import com.liferay.headless.builder.application.APIApplication;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MapUtil;
-import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.odata.entity.CollectionEntityField;
+import com.liferay.portal.odata.entity.ComplexEntityField;
 import com.liferay.portal.odata.entity.EntityField;
 import com.liferay.portal.odata.entity.EntityModel;
 
@@ -39,30 +41,22 @@ public class APISchemaEntityModel implements EntityModel {
 
 		Map<String, EntityField> entityFieldsMap = new HashMap<>();
 
-		for (Map.Entry<String, EntityField> entry :
-				entityModelEntityFieldsMap.entrySet()) {
+		for (APIApplication.Property property : _schema.getProperties()) {
+			EntityField entityField = _findDeepestEntityField(
+				entityModelEntityFieldsMap, property);
 
-			for (APIApplication.Property property : _schema.getProperties()) {
-				if (!StringUtil.equals(
-						property.getSourceFieldName(), entry.getKey())) {
-
-					continue;
-				}
-
-				EntityField entityField = entry.getValue();
+			if ((entityField != null) &&
+				!(entityField instanceof ComplexEntityField)) {
 
 				entityFieldsMap.put(
 					property.getName(),
 					_toAPIPropertyEntityField(entityField, property));
-
-				break;
 			}
 		}
 
 		return entityFieldsMap;
 	}
 
-	@Override
 	public Map<String, EntityRelationship> getEntityRelationshipsMap() {
 		return _entityModel.getEntityRelationshipsMap();
 	}
@@ -70,6 +64,32 @@ public class APISchemaEntityModel implements EntityModel {
 	@Override
 	public String getName() {
 		return _entityModel.getName();
+	}
+
+	private EntityField _findDeepestEntityField(
+		Map<String, EntityField> entityFieldsMap,
+		APIApplication.Property property) {
+
+		if (ListUtil.isEmpty(property.getObjectRelationshipNames())) {
+			return entityFieldsMap.get(property.getSourceFieldName());
+		}
+
+		EntityField entityField = null;
+
+		for (String objectRelationshipName :
+				property.getObjectRelationshipNames()) {
+
+			entityField = entityFieldsMap.get(objectRelationshipName);
+
+			if (!(entityField instanceof ComplexEntityField)) {
+				return null;
+			}
+
+			entityFieldsMap =
+				((ComplexEntityField)entityField).getEntityFieldsMap();
+		}
+
+		return entityFieldsMap.get(property.getSourceFieldName());
 	}
 
 	private EntityField _toAPIPropertyEntityField(
@@ -87,9 +107,23 @@ public class APISchemaEntityModel implements EntityModel {
 		}
 
 		return new APIPropertyEntityField(
-			entityField.getName(), property.getName(), entityField.getType(),
-			locale -> property.getName(), locale -> property.getName(),
-			locale -> property.getName());
+			_toInternalFieldName(entityField, property), property.getName(),
+			entityField.getType(), locale -> property.getName(),
+			locale -> property.getName(), locale -> property.getName());
+	}
+
+	private String _toInternalFieldName(
+		EntityField entityField, APIApplication.Property property) {
+
+		if (ListUtil.isEmpty(property.getObjectRelationshipNames())) {
+			return entityField.getName();
+		}
+
+		String prefix = ListUtil.toString(
+			property.getObjectRelationshipNames(), (String)null,
+			StringPool.SLASH);
+
+		return prefix + StringPool.SLASH + entityField.getName();
 	}
 
 	private final EntityModel _entityModel;
