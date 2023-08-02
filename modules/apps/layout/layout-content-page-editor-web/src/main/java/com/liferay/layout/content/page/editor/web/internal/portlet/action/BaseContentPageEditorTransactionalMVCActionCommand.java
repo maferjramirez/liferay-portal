01a098e138a2cbcdfd39f6bf562c60f8b1ebdfc3
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-08-02 16:21:04 +0200

    LPS-191350 Extract common code to a util class

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/BaseContentPageEditorTransactionalMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/BaseContentPageEditorTransactionalMVCActionCommand.java
@@ -7,40 +7,27 @@ package com.liferay.layout.content.page.editor.web.internal.portlet.action;
 
 import com.liferay.fragment.exception.FragmentCompositionDescriptionException;
 import com.liferay.fragment.exception.FragmentCompositionNameException;
-import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
+import com.liferay.layout.content.page.editor.web.internal.util.LayoutLockManager;
 import com.liferay.portal.kernel.exception.LockedLayoutException;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.PortletIdException;
-import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
-import com.liferay.portal.kernel.lock.Lock;
-import com.liferay.portal.kernel.lock.LockManagerUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
-import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.TransactionConfig;
 import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portal.model.impl.LayoutModelImpl;
 
 import java.util.concurrent.Callable;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.PortletRequest;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Eudaldo Alonso
@@ -57,7 +44,7 @@ public abstract class BaseContentPageEditorTransactionalMVCActionCommand
 
 		try {
 			if (isLayoutLockRequired()) {
-				_getLayoutLock(actionRequest);
+				LayoutLockManager.getLock(actionRequest);
 			}
 
 			Callable<JSONObject> callable = () -> doTransactionalCommand(
@@ -131,67 +118,7 @@ public abstract class BaseContentPageEditorTransactionalMVCActionCommand
 
 		return JSONUtil.put(
 			"redirectURL",
-			() -> PortletURLBuilder.create(
-				PortalUtil.getControlPanelPortletURL(
-					actionRequest, LayoutAdminPortletKeys.GROUP_PAGES,
-					PortletRequest.RENDER_PHASE)
-			).setMVCRenderCommandName(
-				"/layout_admin/locked_layout"
-			).setBackURL(
-				() -> {
-					String backURL = ParamUtil.getString(
-						actionRequest, "backURL");
-
-					if (Validator.isNotNull(backURL)) {
-						return backURL;
-					}
-
-					HttpServletRequest httpServletRequest =
-						PortalUtil.getHttpServletRequest(actionRequest);
-
-					backURL = ParamUtil.getString(
-						httpServletRequest, "p_l_back_url");
-
-					if (Validator.isNotNull(backURL)) {
-						return backURL;
-					}
-
-					return ParamUtil.getString(httpServletRequest, "redirect");
-				}
-			).buildString());
-	}
-
-	private void _getLayoutLock(ActionRequest actionRequest)
-		throws PortalException {
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		Layout layout = themeDisplay.getLayout();
-
-		if (!FeatureFlagManagerUtil.isEnabled("LPS-180328") ||
-			!layout.isDraftLayout()) {
-
-			return;
-		}
-
-		Lock lock = LockManagerUtil.fetchLock(
-			Layout.class.getName(), layout.getPlid());
-
-		if (lock == null) {
-			try {
-				LockManagerUtil.lock(
-					themeDisplay.getUserId(), Layout.class.getName(),
-					layout.getPlid(), null, false,
-					LayoutModelImpl.LOCK_EXPIRATION_TIME);
-			}
-			catch (PortalException portalException) {
-				throw new LockedLayoutException(portalException);
-			}
-		}
-		else if (lock.getUserId() != themeDisplay.getUserId()) {
-			throw new LockedLayoutException();
-		}
+			() -> LayoutLockManager.getBlockedPageURL(actionRequest));
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
