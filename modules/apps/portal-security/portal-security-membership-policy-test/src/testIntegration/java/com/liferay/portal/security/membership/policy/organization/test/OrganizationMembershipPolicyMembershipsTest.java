434cf3762f53a1a92a821524e981a2922e3662a7
Jessie Zhao <jessie.zhao@liferay.com>
2023-08-03 14:42:59 +0800

    LRQA-81639 Ignore integration tests due to LPS-189972

--- modules/apps/portal-security/portal-security-membership-policy-test/src/testIntegration/java/com/liferay/portal/security/membership/policy/organization/test/OrganizationMembershipPolicyMembershipsTest.java
+++ modules/apps/portal-security/portal-security-membership-policy-test/src/testIntegration/java/com/liferay/portal/security/membership/policy/organization/test/OrganizationMembershipPolicyMembershipsTest.java
@@ -30,6 +30,7 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -118,6 +119,7 @@ public class OrganizationMembershipPolicyMembershipsTest
 			Collections.<UserGroupRole>emptyList());
 	}
 
+	@Ignore
 	@Test
 	public void testAssignUserToRequiredOrganizations() throws Exception {
 		long[] userIds = addUsers();
