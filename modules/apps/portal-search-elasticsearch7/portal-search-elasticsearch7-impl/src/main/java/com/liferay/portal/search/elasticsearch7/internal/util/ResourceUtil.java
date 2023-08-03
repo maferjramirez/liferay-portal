65662c0cc4fbd94c06f19a0311a69a4a0acb06d1
Bryan Engler <bryan.engler@liferay.com>
2023-08-03 10:08:51 -0700

    LPS-191281 LPS-192424 portal-search-elasticsearch7-impl: Implement
    https://liferay.atlassian.net/browse/LPS-192424

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/util/ResourceUtil.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/util/ResourceUtil.java
@@ -9,6 +9,15 @@ import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.InputStream;
 
+import java.net.URL;
+
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
 /**
  * @author Michael C. Han
  */
@@ -26,6 +35,27 @@ public class ResourceUtil {
 			throw new RuntimeException(
 				"Unable to load resource: " + resourceName, exception);
 		}
+	}
+
+	public static Set<String> getResourcesAsStrings(
+		BundleContext bundleContext, Class<?> clazz, String directory) {
+
+		Set<String> resources = new HashSet<>();
+
+		Bundle bundle = bundleContext.getBundle();
+
+		Enumeration<URL> enumeration = bundle.findEntries(
+			directory, "*.json", true);
+
+		if (enumeration != null) {
+			while (enumeration.hasMoreElements()) {
+				URL url = enumeration.nextElement();
+
+				resources.add(getResourceAsString(clazz, url.getFile()));
+			}
+		}
+
+		return resources;
 	}
 
 }
