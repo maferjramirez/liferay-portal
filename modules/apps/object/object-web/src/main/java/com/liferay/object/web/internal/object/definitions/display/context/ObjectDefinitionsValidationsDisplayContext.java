6e8cf13f492326517e6451f5921160e478b76488
Mateus Santana <mateus.santana@liferay.com>
2023-08-04 14:42:55 -0300

    LPS-190134 Rename

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/display/context/ObjectDefinitionsValidationsDisplayContext.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/display/context/ObjectDefinitionsValidationsDisplayContext.java
@@ -95,9 +95,9 @@ public class ObjectDefinitionsValidationsDisplayContext
 					"label",
 					LanguageUtil.get(
 						objectRequestHelper.getLocale(),
-						objectValidationRuleEngine.getName())
+						objectValidationRuleEngine.getLabel())
 				).put(
-					"name", objectValidationRuleEngine.getName()
+					"name", objectValidationRuleEngine.getLabel()
 				).build()),
 			Comparator.comparing(item -> item.get("label")));
 	}
