56cc2a0414dba678ec9ad3d591802e35669e0b6e
Pedro Leite <pedro.leite@liferay.com>
2023-08-04 13:34:37 -0300

    LPS-188997 Create test to Organization and User with relationships

---
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/related/models/test/OrganizationSystemObjectRelatedModelsProviderTest.java
@@ -0,0 +1,85 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.related.models.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.portal.kernel.exception.NoSuchOrganizationException;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.OrganizationConstants;
+import com.liferay.portal.kernel.service.OrganizationLocalService;
+import com.liferay.portal.kernel.test.AssertUtils;
+import com.liferay.portal.kernel.test.util.OrganizationTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.Inject;
+
+import org.junit.runner.RunWith;
+
+/**
+ * @author Pedro Leite
+ */
+@RunWith(Arquillian.class)
+public class OrganizationSystemObjectRelatedModelsProviderTest
+	extends BaseSystemObjectRelatedModelsProviderTestCase {
+
+	@Override
+	protected long[] addSystemObjectEntry(int count) throws Exception {
+		long[] organizationIds = new long[count];
+
+		for (int i = 0; i < count; i++) {
+			Organization organization = OrganizationTestUtil.addOrganization(
+				OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID,
+				RandomTestUtil.randomString(), false);
+
+			organizationIds[i] = organization.getOrganizationId();
+		}
+
+		return organizationIds;
+	}
+
+	@Override
+	protected void assertFailureNoSuchException(long primaryKey) {
+		AssertUtils.assertFailure(
+			NoSuchOrganizationException.class,
+			"No Organization exists with the primary key " + primaryKey,
+			() -> _organizationLocalService.getOrganization(primaryKey));
+	}
+
+	@Override
+	protected void deleteSystemObjectEntry(long primaryKey) throws Exception {
+		_organizationLocalService.deleteOrganization(primaryKey);
+	}
+
+	@Override
+	protected Object fetchSystemObjectEntry(long primaryKey) {
+		return _organizationLocalService.fetchOrganization(primaryKey);
+	}
+
+	@Override
+	protected ObjectDefinition getSystemObjectDefinition() throws Exception {
+		return _objectDefinitionLocalService.fetchObjectDefinitionByClassName(
+			TestPropsValues.getCompanyId(), Organization.class.getName());
+	}
+
+	@Override
+	protected String getSystemObjectEntryName(long primaryKey)
+		throws Exception {
+
+		Organization organization = _organizationLocalService.getOrganization(
+			primaryKey);
+
+		return organization.getName();
+	}
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Inject
+	private OrganizationLocalService _organizationLocalService;
+
+}
---
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/related/models/test/UserSystemObjectRelatedModelsProviderTest.java
@@ -0,0 +1,80 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.related.models.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.portal.kernel.exception.NoSuchUserException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.test.AssertUtils;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.test.rule.Inject;
+
+import org.junit.runner.RunWith;
+
+/**
+ * @author Pedro Leite
+ */
+@RunWith(Arquillian.class)
+public class UserSystemObjectRelatedModelsProviderTest
+	extends BaseSystemObjectRelatedModelsProviderTestCase {
+
+	@Override
+	protected long[] addSystemObjectEntry(int count) throws Exception {
+		long[] userIds = new long[count];
+
+		for (int i = 0; i < count; i++) {
+			User user = UserTestUtil.addUser();
+
+			userIds[i] = user.getUserId();
+		}
+
+		return userIds;
+	}
+
+	@Override
+	protected void assertFailureNoSuchException(long primaryKey) {
+		AssertUtils.assertFailure(
+			NoSuchUserException.class,
+			"No User exists with the primary key " + primaryKey,
+			() -> _userLocalService.getUser(primaryKey));
+	}
+
+	@Override
+	protected void deleteSystemObjectEntry(long primaryKey) throws Exception {
+		_userLocalService.deleteUser(primaryKey);
+	}
+
+	@Override
+	protected Object fetchSystemObjectEntry(long primaryKey) {
+		return _userLocalService.fetchUser(primaryKey);
+	}
+
+	@Override
+	protected ObjectDefinition getSystemObjectDefinition() throws Exception {
+		return _objectDefinitionLocalService.fetchObjectDefinitionByClassName(
+			TestPropsValues.getCompanyId(), User.class.getName());
+	}
+
+	@Override
+	protected String getSystemObjectEntryName(long primaryKey)
+		throws Exception {
+
+		User user = _userLocalService.getUser(primaryKey);
+
+		return user.getFirstName();
+	}
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Inject
+	private UserLocalService _userLocalService;
+
+}
