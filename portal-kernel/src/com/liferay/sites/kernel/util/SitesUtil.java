34d4208e58382a25ca3eed72cc5a5a8d82887efa
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2023-08-05 16:59:57 +0100

    LPS-192910 Removes unused methods

--- portal-kernel/src/com/liferay/sites/kernel/util/SitesUtil.java
+++ portal-kernel/src/com/liferay/sites/kernel/util/SitesUtil.java
@@ -10,24 +10,7 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutPrototype;
 import com.liferay.portal.kernel.model.LayoutSet;
-import com.liferay.portal.kernel.model.LayoutSetPrototype;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
-import com.liferay.portal.kernel.service.ServiceContext;
-
-import java.io.File;
-import java.io.InputStream;
-
-import java.util.List;
-import java.util.Map;
-
-import javax.portlet.PortletPreferences;
-import javax.portlet.PortletRequest;
-import javax.portlet.PortletResponse;
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author Raymond Augé
@@ -35,12 +18,6 @@ import javax.servlet.http.HttpServletResponse;
  * @author Zsolt Berentey
  */
 public class SitesUtil {
-
-	public static void addMergeFailFriendlyURLLayout(Layout layout)
-		throws PortalException {
-
-		_sites.addMergeFailFriendlyURLLayout(layout);
-	}
 
 	public static void applyLayoutPrototype(
 			LayoutPrototype layoutPrototype, Layout targetLayout,
@@ -50,102 +27,12 @@ public class SitesUtil {
 		_sites.applyLayoutPrototype(layoutPrototype, targetLayout, linkEnabled);
 	}
 
-	public static void copyLayout(
-			long userId, Layout sourceLayout, Layout targetLayout,
-			ServiceContext serviceContext)
-		throws Exception {
-
-		_sites.copyLayout(userId, sourceLayout, targetLayout, serviceContext);
-	}
-
-	public static void copyLookAndFeel(Layout targetLayout, Layout sourceLayout)
-		throws Exception {
-
-		_sites.copyLookAndFeel(targetLayout, sourceLayout);
-	}
-
-	public static void copyPortletPermissions(
-			Layout targetLayout, Layout sourceLayout)
-		throws Exception {
-
-		_sites.copyPortletPermissions(targetLayout, sourceLayout);
-	}
-
-	public static void copyPortletSetups(
-			Layout sourceLayout, Layout targetLayout)
-		throws Exception {
-
-		_sites.copyPortletSetups(sourceLayout, targetLayout);
-	}
-
-	public static void copyTypeSettings(Group sourceGroup, Group targetGroup)
-		throws Exception {
-
-		_sites.copyTypeSettings(sourceGroup, targetGroup);
-	}
-
-	public static Object[] deleteLayout(
-			HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse)
-		throws Exception {
-
-		return _sites.deleteLayout(httpServletRequest, httpServletResponse);
-	}
-
-	public static Object[] deleteLayout(
-			PortletRequest portletRequest, PortletResponse portletResponse)
-		throws Exception {
-
-		return _sites.deleteLayout(portletRequest, portletResponse);
-	}
-
-	public static void deleteLayout(
-			RenderRequest renderRequest, RenderResponse renderResponse)
-		throws Exception {
-
-		_sites.deleteLayout(renderRequest, renderResponse);
-	}
-
-	public static File exportLayoutSetPrototype(
-			LayoutSetPrototype layoutSetPrototype,
-			ServiceContext serviceContext)
-		throws PortalException {
-
-		return _sites.exportLayoutSetPrototype(
-			layoutSetPrototype, serviceContext);
-	}
-
-	public static Long[] filterGroups(List<Group> groups, String[] names) {
-		return _sites.filterGroups(groups, names);
-	}
-
-	public static Map<String, String[]> getLayoutSetPrototypeParameters(
-		ServiceContext serviceContext) {
-
-		return _sites.getLayoutSetPrototypeParameters(serviceContext);
-	}
-
 	public static Sites getSites() {
 		return _sites;
 	}
 
-	public static void importLayoutSetPrototype(
-			LayoutSetPrototype layoutSetPrototype, InputStream inputStream,
-			ServiceContext serviceContext)
-		throws PortalException {
-
-		_sites.importLayoutSetPrototype(
-			layoutSetPrototype, inputStream, serviceContext);
-	}
-
 	public static boolean isContentSharingWithChildrenEnabled(Group group) {
 		return _sites.isContentSharingWithChildrenEnabled(group);
-	}
-
-	public static boolean isLayoutModifiedSinceLastMerge(Layout layout)
-		throws PortalException {
-
-		return _sites.isLayoutModifiedSinceLastMerge(layout);
 	}
 
 	public static boolean isLayoutSetMergeable(Group group, LayoutSet layoutSet)
@@ -173,48 +60,6 @@ public class SitesUtil {
 		throws Exception {
 
 		_sites.mergeLayoutSetPrototypeLayouts(group, layoutSet);
-	}
-
-	public static void removeMergeFailFriendlyURLLayouts(LayoutSet layoutSet)
-		throws PortalException {
-
-		_sites.removeMergeFailFriendlyURLLayouts(layoutSet);
-	}
-
-	public static void resetPrototype(Layout layout) throws PortalException {
-		_sites.resetPrototype(layout);
-	}
-
-	public static void resetPrototype(LayoutSet layoutSet)
-		throws PortalException {
-
-		_sites.resetPrototype(layoutSet);
-	}
-
-	public static void setMergeFailCount(
-			LayoutPrototype layoutPrototype, int newMergeFailCount)
-		throws PortalException {
-
-		_sites.setMergeFailCount(layoutPrototype, newMergeFailCount);
-	}
-
-	public static void setMergeFailCount(
-			LayoutSetPrototype layoutSetPrototype, int newMergeFailCount)
-		throws PortalException {
-
-		_sites.setMergeFailCount(layoutSetPrototype, newMergeFailCount);
-	}
-
-	public static void updateLayoutScopes(
-			long userId, Layout sourceLayout, Layout targetLayout,
-			PortletPreferences sourcePreferences,
-			PortletPreferences targetPreferences, String sourcePortletId,
-			String languageId)
-		throws Exception {
-
-		_sites.updateLayoutScopes(
-			userId, sourceLayout, targetLayout, sourcePreferences,
-			targetPreferences, sourcePortletId, languageId);
 	}
 
 	public static void updateLayoutSetPrototypesLinks(
