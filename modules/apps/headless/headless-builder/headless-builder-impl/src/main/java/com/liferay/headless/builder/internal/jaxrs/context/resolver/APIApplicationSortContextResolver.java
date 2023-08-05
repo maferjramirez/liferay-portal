fbfd08bfe63151def046221d520e78c0201b7a3f
Luismi Barcos <luismiguel.barcos@liferay.com>
2023-08-06 01:14:06 +0200

    LPS-192151 Allow users to sort by Schema properties. This only supports properties mapped to the main object definition fields but not with the related objects fields. The properties mapped to the related objects properties will be added in this story https://liferay.atlassian.net/browse/LPS-192589

--- modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/jaxrs/context/resolver/APIApplicationSortContextResolver.java
+++ modules/apps/headless/headless-builder/headless-builder-impl/src/main/java/com/liferay/headless/builder/internal/jaxrs/context/resolver/APIApplicationSortContextResolver.java
@@ -6,16 +6,27 @@
 package com.liferay.headless.builder.internal.jaxrs.context.resolver;
 
 import com.liferay.headless.builder.application.APIApplication;
+import com.liferay.headless.builder.internal.odata.entity.APIPropertyEntityField;
+import com.liferay.headless.builder.internal.odata.entity.APISchemaEntityModel;
 import com.liferay.object.rest.odata.entity.v1_0.provider.EntityModelProvider;
 import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.odata.entity.EntityField;
 import com.liferay.portal.odata.entity.EntityModel;
 import com.liferay.portal.odata.sort.SortParserProvider;
 import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
 import com.liferay.portal.vulcan.util.SortUtil;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ContextResolver;
@@ -40,10 +51,6 @@ public class APIApplicationSortContextResolver
 
 	@Override
 	public Sort[] getContext(Class<?> aClass) {
-		if (_sorts != null) {
-			return _sorts;
-		}
-
 		APIApplication.Schema responseSchema = _endpoint.getResponseSchema();
 
 		try {
@@ -54,11 +61,25 @@ public class APIApplicationSortContextResolver
 							getMainObjectDefinitionExternalReferenceCode(),
 						_company.getCompanyId()));
 
-			return SortUtil.getSorts(
-				contextAcceptLanguage, entityModel,
-				_sortParserProvider.provide(entityModel),
-				_endpoint.getSort(
-				).getODataSortString());
+			String sortString = ParamUtil.getString(
+				_httpServletRequest, "sort");
+
+			if (Validator.isNull(sortString)) {
+				APIApplication.Sort sort = _endpoint.getSort();
+
+				String oDataSortString =
+					(sort != null) ? sort.getODataSortString() : null;
+
+				return SortUtil.getSorts(
+					_contextAcceptLanguage, entityModel,
+					_sortParserProvider.provide(entityModel), oDataSortString);
+			}
+
+			return _getSortsFromParamSortString(
+				entityModel, responseSchema, sortString);
+		}
+		catch (WebApplicationException webApplicationException) {
+			throw webApplicationException;
 		}
 		catch (Exception exception) {
 			throw new ServerErrorException(
@@ -66,16 +87,46 @@ public class APIApplicationSortContextResolver
 		}
 	}
 
-	@Context
-	protected AcceptLanguage contextAcceptLanguage;
+	private Sort[] _getSortsFromParamSortString(
+		EntityModel entityModel, APIApplication.Schema schema,
+		String sortString) {
+
+		APISchemaEntityModel apiSchemaEntityModel = new APISchemaEntityModel(
+			entityModel, schema);
+
+		Map<String, EntityField> entityFieldsMap =
+			apiSchemaEntityModel.getEntityFieldsMap();
+
+		return TransformUtil.transform(
+			SortUtil.getSorts(
+				_contextAcceptLanguage, apiSchemaEntityModel,
+				_sortParserProvider.provide(apiSchemaEntityModel), sortString),
+			sort -> {
+				APIPropertyEntityField apiPropertyEntityField =
+					(APIPropertyEntityField)entityFieldsMap.get(
+						sort.getFieldName());
+
+				sort.setFieldName(apiPropertyEntityField.getInternalName());
+
+				return sort;
+			},
+			Sort.class);
+	}
 
 	@Context
 	private Company _company;
 
 	@Context
+	private AcceptLanguage _contextAcceptLanguage;
+
+	@Context
 	private APIApplication.Endpoint _endpoint;
 
 	private final EntityModelProvider _entityModelProvider;
+
+	@Context
+	private HttpServletRequest _httpServletRequest;
+
 	private final ObjectDefinitionLocalService _objectDefinitionLocalService;
 	private final SortParserProvider _sortParserProvider;
 
