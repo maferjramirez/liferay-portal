0744a2ec344a7f1b0a736b34e3055771be77999d
Brian Chan <brian.chan@liferay.com>
2023-08-07 12:56:25 -0300

    LPS-190479 Wordsmith

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ViewObjectDefinitions/ViewObjectDefinitions.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ViewObjectDefinitions/ViewObjectDefinitions.tsx
@@ -154,7 +154,7 @@ export default function ViewObjectDefinitions({
 		},
 		emptyState: {
 			description: Liferay.Language.get(
-				'create-your-first-object-or-import-an-existing-one-to-start-working-with-object-folder'
+				'create-your-first-object-or-import-an-existing-one-to-start-working-with-object-folders'
 			),
 			image: '/states/empty_state.gif',
 			title: Liferay.Language.get('no-objects-created-yet'),
