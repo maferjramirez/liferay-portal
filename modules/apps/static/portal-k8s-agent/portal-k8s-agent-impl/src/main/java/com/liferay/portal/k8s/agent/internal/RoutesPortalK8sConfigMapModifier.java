23a12684960c541922d06e615e8ef3f79e131e50
Brian Chan <brian.chan@liferay.com>
2023-08-08 15:51:19 -0300

    LPS-187069 SF

--- modules/apps/static/portal-k8s-agent/portal-k8s-agent-impl/src/main/java/com/liferay/portal/k8s/agent/internal/RoutesPortalK8sConfigMapModifier.java
+++ modules/apps/static/portal-k8s-agent/portal-k8s-agent-impl/src/main/java/com/liferay/portal/k8s/agent/internal/RoutesPortalK8sConfigMapModifier.java
@@ -322,25 +322,24 @@ public class RoutesPortalK8sConfigMapModifier
 		List<String> lxcDXPDomains = StringUtil.split(
 			labels.get("com.liferay.lxc.dxp.domains"), CharPool.NEW_LINE);
 
-		if (!lxcDXPDomains.isEmpty()) {
-			List<String> updatedLXCDXPDomains = new ArrayList<>();
-
-			for (String lxcDXPDomain : lxcDXPDomains) {
-				if ((lxcDXPDomain != null) &&
-					(lxcDXPDomain.indexOf(":") == -1)) {
-
-					updatedLXCDXPDomains.add(
-						lxcDXPDomain + ":" + portalLocalPort);
-				}
-				else {
-					updatedLXCDXPDomains.add(lxcDXPDomain);
-				}
-			}
-
-			labels.put(
-				"com.liferay.lxc.dxp.domains",
-				StringUtil.merge(updatedLXCDXPDomains, StringPool.NEW_LINE));
+		if (lxcDXPDomains.isEmpty()) {
+			return;
 		}
+
+		List<String> updatedLXCDXPDomains = new ArrayList<>();
+
+		for (String lxcDXPDomain : lxcDXPDomains) {
+			if ((lxcDXPDomain != null) && (lxcDXPDomain.indexOf(":") == -1)) {
+				updatedLXCDXPDomains.add(lxcDXPDomain + ":" + portalLocalPort);
+			}
+			else {
+				updatedLXCDXPDomains.add(lxcDXPDomain);
+			}
+		}
+
+		labels.put(
+			"com.liferay.lxc.dxp.domains",
+			StringUtil.merge(updatedLXCDXPDomains, StringPool.NEW_LINE));
 	}
 
 	private void _write(Path path, Map<String, String> data) {
