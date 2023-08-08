c4fe6014f6a0e86b80a8da74181c948a29358eb4
mikellorza <mikel.lorza@liferay.com>
2023-08-08 15:33:03 +0200

    LPS-191374 Change default value to false of the manual membership as a recommended by the security team

--- modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/action/AddGroupMVCActionCommand.java
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/action/AddGroupMVCActionCommand.java
@@ -206,7 +206,7 @@ public class AddGroupMVCActionCommand extends BaseMVCActionCommand {
 		String friendlyURL = ParamUtil.getString(
 			actionRequest, "groupFriendlyURL");
 		boolean manualMembership = ParamUtil.getBoolean(
-			actionRequest, "manualMembership", true);
+			actionRequest, "manualMembership");
 		boolean inheritContent = ParamUtil.getBoolean(
 			actionRequest, "inheritContent");
 		boolean active = ParamUtil.getBoolean(actionRequest, "active", true);
