72f086de6ef0f92207385a28fddaa9eccb1a0252
Joao Victor <joao.victor@liferay.com>
2023-08-09 17:18:27 -0300

    LPS-190147 Move DLOpenerGoogleDriveMimeTypes from document-library-opener-google-drive-api to document-library-opener-google-drive-web

--- modules/apps/document-library-opener/document-library-opener-google-drive-web/src/main/java/com/liferay/document/library/opener/google/drive/web/internal/constants/DLOpenerGoogleDriveMimeTypes.java
+++ modules/apps/document-library-opener/document-library-opener-google-drive-web/src/main/java/com/liferay/document/library/opener/google/drive/web/internal/constants/DLOpenerGoogleDriveMimeTypes.java
@@ -1,9 +1,9 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.document.library.opener.google.drive.constants;
+package com.liferay.document.library.opener.google.drive.web.internal.constants;
 
 import com.liferay.document.library.opener.constants.DLOpenerMimeTypes;
 import com.liferay.petra.string.StringBundler;
