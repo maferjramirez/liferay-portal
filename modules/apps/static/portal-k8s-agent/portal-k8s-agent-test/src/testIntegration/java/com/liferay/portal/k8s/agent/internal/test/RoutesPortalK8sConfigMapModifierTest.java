8adf85d11b7f2e6d3b9cac47614a2b151228db60
Brian Chan <brian.chan@liferay.com>
2023-08-10 20:14:25 -0300

    Revert "LPS-187069 Like this"
    This reverts commit 2b92a67aef3fb0dede4fe5400a5fef15370b75e7.

--- modules/apps/static/portal-k8s-agent/portal-k8s-agent-test/src/testIntegration/java/com/liferay/portal/k8s/agent/internal/test/RoutesPortalK8sConfigMapModifierTest.java
+++ modules/apps/static/portal-k8s-agent/portal-k8s-agent-test/src/testIntegration/java/com/liferay/portal/k8s/agent/internal/test/RoutesPortalK8sConfigMapModifierTest.java
@@ -90,20 +90,53 @@ public class RoutesPortalK8sConfigMapModifierTest {
 	}
 
 	@Test
-	public void testDXPRoutes() throws Exception {
-		_testDXPRoutes(TestPropsValues.getCompanyId(), "localhost", "default");
+	public void testDXPRoutesDefault() throws Exception {
+		Path dxpPath = Paths.get(
+			PropsUtil.get(PropsKeys.LIFERAY_HOME), "routes/default/dxp");
 
+		Assert.assertTrue(Files.exists(dxpPath));
+
+		File dxpDir = dxpPath.toFile();
+
+		String[] fileNames = dxpDir.list();
+
+		Assert.assertEquals(Arrays.toString(fileNames), 4, fileNames.length);
+
+		Path lxcDXPMainDomainPath = dxpPath.resolve(
+			"com.liferay.lxc.dxp.main.domain");
+
+		Assert.assertTrue(Files.exists(lxcDXPMainDomainPath));
+		Assert.assertEquals(
+			"localhost:8080",
+			new String(Files.readAllBytes(lxcDXPMainDomainPath)));
+
+		VirtualHost virtualHost = _virtualHostLocalService.createVirtualHost(
+			_counterLocalService.increment());
+
+		virtualHost.setCompanyId(TestPropsValues.getCompanyId());
+		virtualHost.setHostname("baker.com");
+
+		_virtualHostLocalService.addVirtualHost(virtualHost);
+
+		List<String> lxcDXPDomains = StringUtil.split(
+			new String(
+				Files.readAllBytes(
+					dxpPath.resolve("com.liferay.lxc.dxp.domains"))),
+			CharPool.NEW_LINE);
+
+		Assert.assertEquals(lxcDXPDomains.toString(), 2, lxcDXPDomains.size());
+
+		Assert.assertEquals("localhost:8080", lxcDXPDomains.get(0));
+		Assert.assertEquals("baker.com:8080", lxcDXPDomains.get(1));
+	}
+
+	@Test
+	public void testDXPRoutesNondefault() throws Exception {
 		String webId = "able.com";
 
 		Company company = _companyLocalService.addCompany(
 			null, webId, webId, webId, 0, true, null, null, null, null, null,
 			null);
-
-		_testDXPRoutes(company.getCompanyId(), webId, webId);
-	}
-
-	private void _testDXPRoutes(long companyId, String hostname, String webId)
-		throws Exception {
 
 		Path dxpPath = Paths.get(
 			PropsUtil.get(PropsKeys.LIFERAY_HOME), "routes", webId, "dxp");
@@ -127,7 +160,7 @@ public class RoutesPortalK8sConfigMapModifierTest {
 		VirtualHost virtualHost = _virtualHostLocalService.createVirtualHost(
 			_counterLocalService.increment());
 
-		virtualHost.setCompanyId(companyId);
+		virtualHost.setCompanyId(company.getCompanyId());
 		virtualHost.setHostname("baker.com");
 
 		_virtualHostLocalService.addVirtualHost(virtualHost);
@@ -140,7 +173,7 @@ public class RoutesPortalK8sConfigMapModifierTest {
 
 		Assert.assertEquals(lxcDXPDomains.toString(), 2, lxcDXPDomains.size());
 
-		Assert.assertEquals(hostname + ":8080", lxcDXPDomains.get(0));
+		Assert.assertEquals("able.com:8080", lxcDXPDomains.get(0));
 		Assert.assertEquals("baker.com:8080", lxcDXPDomains.get(1));
 	}
 
