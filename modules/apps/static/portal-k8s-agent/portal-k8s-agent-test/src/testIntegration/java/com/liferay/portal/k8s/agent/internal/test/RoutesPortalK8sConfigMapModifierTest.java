ef255f650eaf9d5df9dc7d63c98f9863084fba63
Brian Chan <brian.chan@liferay.com>
2023-08-10 20:14:33 -0300

    Revert "LPS-187069 Dont' use -1"
    This reverts commit c9c19c6a5137d550c132799eedf45ae9b8ecc3f8.

--- modules/apps/static/portal-k8s-agent/portal-k8s-agent-test/src/testIntegration/java/com/liferay/portal/k8s/agent/internal/test/RoutesPortalK8sConfigMapModifierTest.java
+++ modules/apps/static/portal-k8s-agent/portal-k8s-agent-test/src/testIntegration/java/com/liferay/portal/k8s/agent/internal/test/RoutesPortalK8sConfigMapModifierTest.java
@@ -6,7 +6,6 @@
 package com.liferay.portal.k8s.agent.internal.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.counter.kernel.service.CounterLocalService;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
@@ -119,8 +118,7 @@ public class RoutesPortalK8sConfigMapModifierTest {
 			null, webId, webId, webId, 0, true, null, null, null, null, null,
 			null);
 
-		_virtualHost = _virtualHostLocalService.createVirtualHost(
-			_counterLocalService.increment());
+		_virtualHost = _virtualHostLocalService.createVirtualHost(-1);
 
 		_virtualHost.setCompanyId(_company.getCompanyId());
 		_virtualHost.setHostname("foobar.com");
@@ -256,10 +254,6 @@ public class RoutesPortalK8sConfigMapModifierTest {
 			_serviceTracker;
 
 	private Company _company;
-
-	@Inject
-	private CounterLocalService _counterLocalService;
-
 	private VirtualHost _virtualHost;
 
 	@Inject
