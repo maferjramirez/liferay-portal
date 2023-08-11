9395509b0ffb63ebadfd3b8655ebad1f40ff138c
Janis Zhang <janis.zhang@liferay.com>
2023-08-11 15:56:16 +0800

    LPS-187880 Remove getSettingsFactory method and ServiceProxyFactory blocking usages in SettingsFactoryUtil as no one is using it and Rename to FallbackKeysSettingsUtil, as it's applying FallbackKeys to settings

--- portal-kernel/src/com/liferay/portal/kernel/settings/FallbackKeysSettingsUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/settings/FallbackKeysSettingsUtil.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -9,13 +9,12 @@ import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.portlet.PortletIdCodec;
-import com.liferay.portal.kernel.util.ServiceProxyFactory;
 
 /**
  * @author Raymond Augé
  * @author Jorge Ferrer
  */
-public class SettingsFactoryUtil {
+public class FallbackKeysSettingsUtil {
 
 	public static Settings getSettings(SettingsLocator settingsLocator)
 		throws SettingsException {
@@ -40,18 +39,10 @@ public class SettingsFactoryUtil {
 		return settings;
 	}
 
-	public static SettingsFactory getSettingsFactory() {
-		return _settingsFactory;
-	}
-
 	private static final ServiceTrackerMap<String, FallbackKeys>
 		_fallbackKeysServiceTrackerMap =
 			ServiceTrackerMapFactory.openSingleValueMap(
 				SystemBundleUtil.getBundleContext(), FallbackKeys.class,
 				"settingsId");
-	private static volatile SettingsFactory _settingsFactory =
-		ServiceProxyFactory.newServiceTrackedInstance(
-			SettingsFactory.class, SettingsFactoryUtil.class,
-			"_settingsFactory", true);
 
 }
