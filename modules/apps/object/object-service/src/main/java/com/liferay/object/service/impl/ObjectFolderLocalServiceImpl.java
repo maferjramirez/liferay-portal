b6649bad3f6970d3b767c45071ea77197b78cb89
Murilo Stodolni <murilo.stodolni@liferay.com>
2023-08-11 12:11:08 -0300

    LPS=191513 Reindex all definitions inside folder when update objectFolderERC

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFolderLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFolderLocalServiceImpl.java
@@ -20,6 +20,8 @@ import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.service.ResourceLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
@@ -171,7 +173,19 @@ public class ObjectFolderLocalServiceImpl
 		objectFolder.setExternalReferenceCode(externalReferenceCode);
 		objectFolder.setLabelMap(labelMap, LocaleUtil.getSiteDefault());
 
-		return objectFolderPersistence.update(objectFolder);
+		objectFolder = objectFolderPersistence.update(objectFolder);
+
+		for (ObjectDefinition objectDefinition :
+				_objectDefinitionPersistence.findByObjectFolderId(
+					objectFolder.getObjectFolderId())) {
+
+			Indexer<ObjectDefinition> indexer =
+				IndexerRegistryUtil.nullSafeGetIndexer(ObjectDefinition.class);
+
+			indexer.reindex(objectDefinition);
+		}
+
+		return objectFolder;
 	}
 
 	private void _validateLabel(Map<Locale, String> labelMap)
