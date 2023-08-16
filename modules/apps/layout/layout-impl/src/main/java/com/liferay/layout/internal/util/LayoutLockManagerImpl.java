cc2e3769e30cecf2ef7e802caf11da1eacbd7bf9
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-08-16 11:12:55 +0200

    LPS-193334 Implements getLockedLayouts using DSLQuery

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutLockManagerImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutLockManagerImpl.java
@@ -8,22 +8,31 @@ package com.liferay.layout.internal.util;
 import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
 import com.liferay.layout.model.LockedLayout;
 import com.liferay.layout.util.LayoutLockManager;
+import com.liferay.petra.sql.dsl.DSLFunctionFactoryUtil;
+import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
 import com.liferay.portal.kernel.exception.LockedLayoutException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.lock.Lock;
 import com.liferay.portal.kernel.lock.LockManager;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.LayoutTable;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.lock.model.LockTable;
 import com.liferay.portal.model.impl.LayoutModelImpl;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.portlet.ActionRequest;
@@ -74,7 +83,52 @@ public class LayoutLockManagerImpl implements LayoutLockManager {
 
 	@Override
 	public List<LockedLayout> getLockedLayouts(long groupId) {
-		return Collections.emptyList();
+		List<Object[]> results = _layoutLocalService.dslQuery(
+			DSLQueryFactoryUtil.select(
+				LayoutTable.INSTANCE.classPK, LockTable.INSTANCE.createDate,
+				LayoutTable.INSTANCE.name, LayoutTable.INSTANCE.plid,
+				LayoutTable.INSTANCE.type, LockTable.INSTANCE.userName
+			).from(
+				LayoutTable.INSTANCE
+			).innerJoinON(
+				LockTable.INSTANCE,
+				LockTable.INSTANCE.key.eq(
+					DSLFunctionFactoryUtil.castText(LayoutTable.INSTANCE.plid))
+			).where(
+				LayoutTable.INSTANCE.groupId.eq(
+					groupId
+				).and(
+					LayoutTable.INSTANCE.hidden.eq(true)
+				).and(
+					LayoutTable.INSTANCE.system.eq(true)
+				).and(
+					LayoutTable.INSTANCE.status.eq(
+						WorkflowConstants.STATUS_DRAFT)
+				).and(
+					LayoutTable.INSTANCE.type.in(
+						new String[] {
+							LayoutConstants.TYPE_COLLECTION,
+							LayoutConstants.TYPE_CONTENT
+						})
+				)
+			).orderBy(
+				orderByStep -> orderByStep.orderBy(
+					LayoutTable.INSTANCE.modifiedDate.descending())
+			));
+
+		List<LockedLayout> lockedLayouts = new ArrayList<>(results.size());
+
+		for (Object[] columns : results) {
+			lockedLayouts.add(
+				new LockedLayout(
+					GetterUtil.getLong(columns[0]), (Date)columns[1],
+					GetterUtil.getString(columns[2]),
+					GetterUtil.getLong(columns[3]),
+					GetterUtil.getString(columns[4]),
+					GetterUtil.getString(columns[5])));
+		}
+
+		return lockedLayouts;
 	}
 
 	@Override
@@ -140,6 +194,9 @@ public class LayoutLockManagerImpl implements LayoutLockManager {
 			Layout.class.getName(), String.valueOf(layout.getPlid()),
 			String.valueOf(userId));
 	}
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
 
 	@Reference
 	private LockManager _lockManager;
