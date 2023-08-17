6196309e4e29528f2d0cbe0c2a857dfdbe6b1bb4
Alberto Chaparro <alberto.chaparro@liferay.com>
2023-08-18 01:35:17 +0200

    LPS-193843 Verifiers must be in internal classes

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/verify/DDMServiceVerifyProcess.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/verify/DDMServiceVerifyProcess.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.dynamic.data.mapping.verify;
+package com.liferay.dynamic.data.mapping.internal.verify;
 
 import com.liferay.dynamic.data.mapping.io.DDMFormValuesDeserializer;
 import com.liferay.dynamic.data.mapping.io.DDMFormValuesDeserializerDeserializeRequest;
