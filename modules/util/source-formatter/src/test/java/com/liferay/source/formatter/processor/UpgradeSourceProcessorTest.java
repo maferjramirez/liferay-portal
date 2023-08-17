a04365383944fe6d516b9683d6811e4d80ee570a
Michael Cavalcanti <michael.cavalcanti@liferay.com>
2023-08-17 10:38:01 -0300

    LPS-193550 Create test for XMLUpgradeCompatibilityVersionCheck

--- modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/UpgradeSourceProcessorTest.java
@@ -286,6 +286,11 @@ public class UpgradeSourceProcessorTest extends BaseSourceProcessorTestCase {
 	}
 
 	@Test
+	public void testXMLUpgradeCompatibilityVersionCheck() throws Exception {
+		test("upgrade/XMLUpgradeCompatibilityVersionCheck.testxml");
+	}
+
+	@Test
 	public void testXMLUpgradeDTDVersionCheck() throws Exception {
 		test("upgrade/XMLUpgradeDTDVersionCheck.testxml");
 	}
