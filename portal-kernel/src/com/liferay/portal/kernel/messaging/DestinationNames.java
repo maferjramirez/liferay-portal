258c19b010ee71c2f99ca64b1d2cdaeb1d23134a
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-08-18 10:08:37 +0800

    LPS-193720 Remove unused DOCUMENT_LIBRARY_SYNC_EVENT_PROCESSOR

--- portal-kernel/src/com/liferay/portal/kernel/messaging/DestinationNames.java
+++ portal-kernel/src/com/liferay/portal/kernel/messaging/DestinationNames.java
@@ -49,9 +49,6 @@ public interface DestinationNames {
 	public static final String DOCUMENT_LIBRARY_RAW_METADATA_PROCESSOR =
 		"liferay/document_library_raw_metadata_processor";
 
-	public static final String DOCUMENT_LIBRARY_SYNC_EVENT_PROCESSOR =
-		"liferay/document_library_sync_event_processor";
-
 	public static final String DOCUMENT_LIBRARY_VIDEO_PROCESSOR =
 		"liferay/document_library_video_processor";
 
