e8a00b811419cc6050db7f1b85c96ccb8a868a2c
Sandro Chinea <sandrochinea@gmail.com>
2023-08-18 11:39:11 +0100

    LPS-193776 Introduce loading and error states

--- modules/apps/layout/layout-reports-web/src/main/resources/META-INF/resources/js/components/PageAudit.js
+++ modules/apps/layout/layout-reports-web/src/main/resources/META-INF/resources/js/components/PageAudit.js
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import ClayAlert from '@clayui/alert';
 import ClayLoadingIndicator from '@clayui/loading-indicator';
 import ClayTabs from '@clayui/tabs';
 import {ExperienceSelector} from '@liferay/layout-js-components-web';
@@ -22,20 +23,34 @@ export const TAB_IDS = {
 };
 
 export default function PageAudit({panelIsOpen}) {
-	const [data, setData] = useState({});
+	const [data, setData] = useState(null);
+	const [loading, setLoading] = useState(true);
+
 	const {layoutReportsDataURL} = useContext(ConstantsContext);
 
 	useEffect(() => {
 		if (panelIsOpen && layoutReportsDataURL) {
-			fetch(layoutReportsDataURL, {method: 'GET'})
+			fetch(layoutReportsDataURL)
 				.then((response) => response.json())
 				.then((data) => setData(data))
-				.catch((error) => console.error(error));
+				.catch((error) => console.error(error))
+				.finally(() => setLoading(false));
 		}
 	}, [layoutReportsDataURL, panelIsOpen]);
 
-	if (!data.tabsData || !data.segmentsExperienceSelectorData) {
-		return null;
+	if (loading) {
+		return <ClayLoadingIndicator displayType="secondary" size="sm" />;
+	}
+
+	if (!data) {
+		return (
+			<ClayAlert
+				displayType="danger"
+				title={Liferay.Language.get('error')}
+			>
+				{Liferay.Language.get('an-unexpected-error-occurred')}
+			</ClayAlert>
+		);
 	}
 
 	const pageSpeedData = data.tabsData.find(
@@ -72,44 +87,38 @@ export function PageAuditBody({children, segments, tabs}) {
 				/>
 			) : null}
 
-			{tabs.length ? (
-				<>
-					<ClayTabs
-						active={activeTab}
-						className="px-2"
-						onActiveChange={setActiveTab}
+			<ClayTabs
+				active={activeTab}
+				className="px-2"
+				onActiveChange={setActiveTab}
+			>
+				{tabs.map((tab, index) => (
+					<ClayTabs.Item
+						id={`tab-${tab.id}`}
+						innerProps={{
+							'aria-controls': `tabpanel-${index}`,
+						}}
+						key={tab.id}
 					>
-						{tabs.map((tab, index) => (
-							<ClayTabs.Item
-								id={`tab-${tab.id}`}
-								innerProps={{
-									'aria-controls': `tabpanel-${index}`,
-								}}
-								key={tab.id}
-							>
-								{Liferay.Language.get(tab.name)}
-							</ClayTabs.Item>
-						))}
-					</ClayTabs>
-					<ClayTabs.Content activeIndex={activeTab} fade>
-						{tabs.map((tab) => (
-							<ClayTabs.TabPane
-								aria-labelledby={`tab-${tab.id}`}
-								className="p-3"
-								key={tab.id}
-							>
-								{tab.id === TAB_IDS.renderTimes ? (
-									<RenderTimes url={tab.url} />
-								) : (
-									cloneElement(children, {url: tab.url})
-								)}
-							</ClayTabs.TabPane>
-						))}
-					</ClayTabs.Content>
-				</>
-			) : (
-				<ClayLoadingIndicator displayType="secondary" size="sm" />
-			)}
+						{Liferay.Language.get(tab.name)}
+					</ClayTabs.Item>
+				))}
+			</ClayTabs>
+			<ClayTabs.Content activeIndex={activeTab} fade>
+				{tabs.map((tab) => (
+					<ClayTabs.TabPane
+						aria-labelledby={`tab-${tab.id}`}
+						className="p-3"
+						key={tab.id}
+					>
+						{tab.id === TAB_IDS.renderTimes ? (
+							<RenderTimes url={tab.url} />
+						) : (
+							cloneElement(children, {url: tab.url})
+						)}
+					</ClayTabs.TabPane>
+				))}
+			</ClayTabs.Content>
 		</>
 	);
 }
