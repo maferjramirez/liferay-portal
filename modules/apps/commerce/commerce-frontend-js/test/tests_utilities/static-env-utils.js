3f52c8c7a63686ea10f62b2013d7f9edefb40d97
Juanjo Fernández <juanjo.fernandez@liferay.com>
2023-08-21 07:45:37 +0200

    LPS-192620 - Test. Mock Language.available

--- modules/apps/commerce/commerce-frontend-js/test/tests_utilities/static-env-utils.js
+++ modules/apps/commerce/commerce-frontend-js/test/tests_utilities/static-env-utils.js
@@ -5,6 +5,21 @@
 
 const Liferay = {
 	Language: {
+		available: {
+			ar_SA: 'Arabic (Saudi Arabia)',
+			ca_ES: 'Catalan (Spain)',
+			de_DE: 'German (Germany)',
+			en_US: 'English (United States)',
+			es_ES: 'Spanish (Spain)',
+			fi_FI: 'Finnish (Finland)',
+			fr_FR: 'French (France)',
+			hu_HU: 'Hungarian (Hungary)',
+			ja_JP: 'Japanese (Japan)',
+			nl_NL: 'Dutch (Netherlands)',
+			pt_BR: 'Portuguese (Brazil)',
+			sv_SE: 'Swedish (Sweden)',
+			zh_CN: 'Chinese (China)',
+		},
 		get: (key) => {
 			let counter = 0;
 
