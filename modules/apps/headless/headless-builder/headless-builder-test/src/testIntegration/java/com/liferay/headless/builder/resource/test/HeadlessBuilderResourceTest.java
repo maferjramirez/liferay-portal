24152778e246343ce7d265194e0b555c31f5e1c0
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-08-21 17:53:58 +0200

    LPS-192151 Moving to the right package

--- modules/apps/headless/headless-builder/headless-builder-test/src/testIntegration/java/com/liferay/headless/builder/resource/test/HeadlessBuilderResourceTest.java
+++ modules/apps/headless/headless-builder/headless-builder-test/src/testIntegration/java/com/liferay/headless/builder/resource/test/HeadlessBuilderResourceTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.headless.builder.application.resource.test;
+package com.liferay.headless.builder.resource.test;
 
 import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
 import com.liferay.headless.builder.application.APIApplication;
