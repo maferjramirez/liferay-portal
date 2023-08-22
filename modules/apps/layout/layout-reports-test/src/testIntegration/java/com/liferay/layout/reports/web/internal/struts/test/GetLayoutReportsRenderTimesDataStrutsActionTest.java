6f2758bc9a175fa297df996eb33b8d11bcd75cc9
mikellorza <mikel.lorza@liferay.com>
2023-08-22 11:34:18 +0200

    LPS-190112 Add test to test get render times data of a content layout with a fragment entry

--- modules/apps/layout/layout-reports-test/src/testIntegration/java/com/liferay/layout/reports/web/internal/struts/test/GetLayoutReportsRenderTimesDataStrutsActionTest.java
+++ modules/apps/layout/layout-reports-test/src/testIntegration/java/com/liferay/layout/reports/web/internal/struts/test/GetLayoutReportsRenderTimesDataStrutsActionTest.java
@@ -7,8 +7,12 @@ package com.liferay.layout.reports.web.internal.struts.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.fragment.constants.FragmentConstants;
+import com.liferay.fragment.model.FragmentCollection;
+import com.liferay.fragment.model.FragmentEntry;
 import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentCollectionLocalService;
 import com.liferay.fragment.service.FragmentEntryLinkLocalService;
+import com.liferay.fragment.service.FragmentEntryLocalService;
 import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
 import com.liferay.layout.test.util.LayoutTestUtil;
@@ -22,17 +26,20 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.struts.StrutsAction;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -65,6 +72,8 @@ public class GetLayoutReportsRenderTimesDataStrutsActionTest {
 	public void setUp() throws Exception {
 		_group = GroupTestUtil.addGroup();
 
+		_layout = LayoutTestUtil.addTypeContentLayout(_group);
+
 		_serviceContext = ServiceContextTestUtil.getServiceContext(
 			_group.getGroupId());
 
@@ -72,30 +81,90 @@ public class GetLayoutReportsRenderTimesDataStrutsActionTest {
 	}
 
 	@Test
-	public void testGetRenderTimesDataOfAContentLayoutWithAPortletFragmentEntry()
+	public void testGetRenderTimesDataOfAContentLayoutWithACreatedFragmentEntry()
 		throws Exception {
 
-		Layout layout = LayoutTestUtil.addTypeContentLayout(_group);
+		FragmentEntry fragmentEntry = _addFragmentEntry(true);
 
-		LayoutPageTemplateStructure layoutPageTemplateStructure =
-			_layoutPageTemplateStructureLocalService.
-				fetchLayoutPageTemplateStructure(
-					layout.getGroupId(), layout.getPlid());
+		FragmentEntryLink fragmentEntryLink =
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0,
+				fragmentEntry.getFragmentEntryId(),
+				_segmentsExperienceLocalService.
+					fetchDefaultSegmentsExperienceId(_layout.getPlid()),
+				_layout.getPlid(), fragmentEntry.getCss(),
+				fragmentEntry.getHtml(), fragmentEntry.getJs(),
+				fragmentEntry.getConfiguration(), null, StringPool.BLANK, 0,
+				null, fragmentEntry.getType(), _serviceContext);
 
-		LayoutStructure layoutStructure = LayoutStructure.of(
-			layoutPageTemplateStructure.getDefaultSegmentsExperienceData());
+		_addFragmentStyledLayoutStructureItem(fragmentEntryLink);
 
-		long defaultSegmentsExperienceId =
-			_segmentsExperienceLocalService.fetchDefaultSegmentsExperienceId(
-				layout.getPlid());
+		JSONArray jsonArray = _serveResource();
+
+		Assert.assertEquals(1, jsonArray.length());
+
+		JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+		Assert.assertNotNull(jsonObject);
+		Assert.assertTrue(jsonObject.getBoolean("cached"));
+		Assert.assertNotNull(jsonObject.get("itemId"));
+		Assert.assertTrue(jsonObject.getBoolean("fragment"));
+		Assert.assertEquals("fragment", jsonObject.getString("itemType"));
+		Assert.assertFalse(jsonObject.getBoolean("fromMaster"));
+
+		String fragmentCollectionURL = jsonObject.getString(
+			"fragmentCollectionURL");
+
+		Assert.assertNotNull(fragmentCollectionURL);
+		Assert.assertTrue(
+			fragmentCollectionURL.contains(
+				"fragmentCollectionId=" +
+					fragmentEntry.getFragmentCollectionId()));
+
+		Assert.assertEquals(
+			fragmentEntry.getName(), jsonObject.getString("hierarchy"));
+		Assert.assertEquals(
+			fragmentEntry.getName(), jsonObject.getString("name"));
+		Assert.assertNotNull(jsonObject.get("renderTime"));
+	}
+
+	@Test
+	public void testGetRenderTimesDataOfAContentLayoutWithACreatedFragmentEntryInsideAColumn()
+		throws Exception {
+
+		FragmentEntry fragmentEntry = _addFragmentEntry(true);
+
+		FragmentEntryLink fragmentEntryLink =
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0,
+				fragmentEntry.getFragmentEntryId(),
+				_segmentsExperienceLocalService.
+					fetchDefaultSegmentsExperienceId(_layout.getPlid()),
+				_layout.getPlid(), fragmentEntry.getCss(),
+				fragmentEntry.getHtml(), fragmentEntry.getJs(),
+				fragmentEntry.getConfiguration(), null, StringPool.BLANK, 0,
+				null, fragmentEntry.getType(), _serviceContext);
+
+		_addFragmentStyledLayoutStructureItem(fragmentEntryLink);
+
+		JSONArray jsonArray = _serveResource();
+
+		Assert.assertEquals(1, jsonArray.length());
+	}
+
+	@Test
+	public void testGetRenderTimesDataOfAContentLayoutWithAPortletFragmentEntry()
+		throws Exception {
 
 		String portletId = "com_liferay_test_portlet_LayoutTestPortlet";
 
 		FragmentEntryLink fragmentEntryLink =
 			_fragmentEntryLinkLocalService.addFragmentEntryLink(
-				layout.getUserId(), _group.getGroupId(), 0, 0,
-				defaultSegmentsExperienceId, layout.getPlid(), StringPool.BLANK,
-				StringPool.BLANK, StringPool.BLANK, StringPool.BLANK,
+				_layout.getUserId(), _group.getGroupId(), 0, 0,
+				_segmentsExperienceLocalService.
+					fetchDefaultSegmentsExperienceId(_layout.getPlid()),
+				_layout.getPlid(), StringPool.BLANK, StringPool.BLANK,
+				StringPool.BLANK, StringPool.BLANK,
 				JSONUtil.put(
 					"instanceid", StringUtil.randomString()
 				).put(
@@ -104,16 +173,9 @@ public class GetLayoutReportsRenderTimesDataStrutsActionTest {
 				StringPool.BLANK, 0, StringPool.BLANK,
 				FragmentConstants.TYPE_PORTLET, _serviceContext);
 
-		layoutStructure.addFragmentStyledLayoutStructureItem(
-			fragmentEntryLink.getFragmentEntryLinkId(),
-			layoutStructure.getMainItemId(), 0);
+		_addFragmentStyledLayoutStructureItem(fragmentEntryLink);
 
-		_layoutPageTemplateStructureLocalService.
-			updateLayoutPageTemplateStructureData(
-				layout.getGroupId(), layout.getPlid(),
-				defaultSegmentsExperienceId, layoutStructure.toString());
-
-		JSONArray jsonArray = _serveResource(layout);
+		JSONArray jsonArray = _serveResource();
 
 		Assert.assertEquals(1, jsonArray.length());
 
@@ -131,16 +193,60 @@ public class GetLayoutReportsRenderTimesDataStrutsActionTest {
 		Assert.assertNotNull(jsonObject.get("renderTime"));
 	}
 
-	private JSONArray _serveResource(Layout layout) throws Exception {
+	private FragmentEntry _addFragmentEntry(boolean cacheable)
+		throws Exception {
+
+		FragmentCollection fragmentCollection =
+			_fragmentCollectionLocalService.addFragmentCollection(
+				TestPropsValues.getUserId(), _group.getGroupId(),
+				RandomTestUtil.randomString(), StringPool.BLANK,
+				_serviceContext);
+
+		return _fragmentEntryLocalService.addFragmentEntry(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			fragmentCollection.getFragmentCollectionId(), null,
+			RandomTestUtil.randomString(), StringPool.BLANK,
+			"Fragment Entry HTML", StringPool.BLANK, cacheable, null, null, 0,
+			FragmentConstants.TYPE_COMPONENT, null,
+			WorkflowConstants.STATUS_APPROVED, _serviceContext);
+	}
+
+	private void _addFragmentStyledLayoutStructureItem(
+			FragmentEntryLink fragmentEntryLink)
+		throws Exception {
+
+		LayoutPageTemplateStructure layoutPageTemplateStructure =
+			_layoutPageTemplateStructureLocalService.
+				fetchLayoutPageTemplateStructure(
+					_layout.getGroupId(), _layout.getPlid());
+
+		LayoutStructure layoutStructure = LayoutStructure.of(
+			layoutPageTemplateStructure.getDefaultSegmentsExperienceData());
+
+		layoutStructure.addFragmentStyledLayoutStructureItem(
+			fragmentEntryLink.getFragmentEntryLinkId(),
+			layoutStructure.getMainItemId(), 0);
+
+		_layoutPageTemplateStructureLocalService.
+			updateLayoutPageTemplateStructureData(
+				_layout.getGroupId(), _layout.getPlid(),
+				_segmentsExperienceLocalService.
+					fetchDefaultSegmentsExperienceId(_layout.getPlid()),
+				layoutStructure.toString());
+	}
+
+	private JSONArray _serveResource() throws Exception {
 		MockHttpServletRequest mockHttpServletRequest =
 			new MockHttpServletRequest();
 
 		mockHttpServletRequest.setParameter(
-			"p_l_id", String.valueOf(layout.getPlid()));
+			"p_l_id", String.valueOf(_layout.getPlid()));
 
 		ThemeDisplay themeDisplay = new ThemeDisplay();
 
-		themeDisplay.setLayout(layout);
+		themeDisplay.setCompany(
+			_companyLocalService.getCompany(_group.getCompanyId()));
+		themeDisplay.setLayout(_layout);
 		themeDisplay.setPermissionChecker(
 			PermissionThreadLocal.getPermissionChecker());
 		themeDisplay.setScopeGroupId(_group.getGroupId());
@@ -164,7 +270,16 @@ public class GetLayoutReportsRenderTimesDataStrutsActionTest {
 	}
 
 	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@Inject
+	private FragmentCollectionLocalService _fragmentCollectionLocalService;
+
+	@Inject
 	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@Inject
+	private FragmentEntryLocalService _fragmentEntryLocalService;
 
 	@Inject(
 		filter = "component.name=com.liferay.layout.reports.web.internal.struts.GetLayoutReportsRenderTimesDataStrutsAction"
@@ -173,6 +288,8 @@ public class GetLayoutReportsRenderTimesDataStrutsActionTest {
 
 	@DeleteAfterTestRun
 	private Group _group;
+
+	private Layout _layout;
 
 	@Inject
 	private LayoutPageTemplateStructureLocalService
