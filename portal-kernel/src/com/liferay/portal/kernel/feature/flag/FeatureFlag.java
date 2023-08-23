a8ddf93d899ca7b173c419eb414ad5320f53bf49
Thiago Buarque <thiago.buarque@liferay.com>
2023-08-23 15:26:28 -0300

    LPS-190476 move FeatureFlag[Type, Constants] to portal-kernel

--- portal-kernel/src/com/liferay/portal/kernel/feature/flag/FeatureFlag.java
+++ portal-kernel/src/com/liferay/portal/kernel/feature/flag/FeatureFlag.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.feature.flag.web.internal.model;
+package com.liferay.portal.kernel.feature.flag;
 
 import java.util.Locale;
 
--- portal-kernel/src/com/liferay/portal/kernel/feature/flag/FeatureFlagType.java
+++ portal-kernel/src/com/liferay/portal/kernel/feature/flag/FeatureFlagType.java
@@ -3,10 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.feature.flag.web.internal.model;
+package com.liferay.portal.kernel.feature.flag;
 
-import com.liferay.feature.flag.web.internal.constants.FeatureFlagConstants;
 import com.liferay.portal.kernel.configuration.Filter;
+import com.liferay.portal.kernel.feature.flag.constants.FeatureFlagConstants;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
