0fcf18b3f0e1d6abbcf12be18dc0775a76bc6aca
shuyangzhou <shuyang.zhou@liferay.com>
2023-08-24 10:11:40 -0700

    Revert "LPS-169817 Re-invoke the AuditFilter after authenticating a request with AuthVerifierFilter"
    This reverts commit 1bf3cef1ded9cd42b74a9933a6fdf8d046aa8376.

--- portal-impl/src/com/liferay/portal/servlet/filters/authverifier/AuthVerifierFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/authverifier/AuthVerifierFilter.java
@@ -9,13 +9,11 @@ import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.security.access.control.AccessControlUtil;
 import com.liferay.portal.kernel.security.auth.AccessControlContext;
 import com.liferay.portal.kernel.security.auth.verifier.AuthVerifierConfiguration;
 import com.liferay.portal.kernel.security.auth.verifier.AuthVerifierResult;
 import com.liferay.portal.kernel.servlet.ServletContextPool;
-import com.liferay.portal.kernel.servlet.filters.invoker.InvokerFilterChain;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HttpComponentsUtil;
 import com.liferay.portal.kernel.util.MapUtil;
@@ -37,14 +35,11 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.osgi.util.tracker.ServiceTracker;
 
 /**
  * <p>
@@ -129,8 +124,6 @@ public class AuthVerifierFilter extends BasePortalFilter {
 					_buildAuthVerifierConfigurations(_initParametersMap),
 					servletContext.getContextPath()));
 		}
-
-		_serviceTracker.open();
 	}
 
 	@Override
@@ -204,19 +197,6 @@ public class AuthVerifierFilter extends BasePortalFilter {
 					httpServletRequest, userId, authType);
 
 			accessControlContext.setRequest(authVerifierServletRequest);
-
-			Filter filter = _serviceTracker.getService();
-
-			if (filter != null) {
-				InvokerFilterChain invokerFilterChain = new InvokerFilterChain(
-					(servletRequest, servletResponse) -> {
-					});
-
-				invokerFilterChain.addFilter(filter);
-
-				invokerFilterChain.doFilter(
-					authVerifierServletRequest, httpServletResponse);
-			}
 
 			Class<?> clazz = getClass();
 
@@ -365,14 +345,6 @@ public class AuthVerifierFilter extends BasePortalFilter {
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		AuthVerifierFilter.class.getName());
-
-	private static final ServiceTracker<Filter, Filter> _serviceTracker =
-		new ServiceTracker<>(
-			SystemBundleUtil.getBundleContext(),
-			SystemBundleUtil.createFilter(
-				"(component.name=com.liferay.portal.security.audit.wiring." +
-					"internal.servlet.filter.AuditFilter)"),
-			null);
 
 	private boolean _guestAllowed = true;
 	private final Set<String> _hostsAllowed = new HashSet<>();
