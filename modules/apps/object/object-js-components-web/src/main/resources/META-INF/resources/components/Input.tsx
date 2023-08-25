bea23b22a56f3e641e03db89a85e3616900a55fc
Rebeca Silva <rebeca.silva@liferay.com>
2023-08-25 18:28:40 -0300

    LPS-193641 Expose readOnly prop on Input Component

--- modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/components/Input.tsx
+++ modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/components/Input.tsx
@@ -20,6 +20,7 @@ export const Input = React.forwardRef<HTMLInputElement, IProps>(
 			name,
 			onChange,
 			onInput,
+			readOnly,
 			required,
 			type,
 			value,
@@ -45,6 +46,7 @@ export const Input = React.forwardRef<HTMLInputElement, IProps>(
 					name={name}
 					onChange={onChange}
 					onInput={onInput}
+					readOnly={readOnly}
 					ref={ref}
 					type={type}
 					value={value}
