045c280592b299e2e59993971ebaaa088c0bb135
pei-jung <pei-jung.lan@liferay.com>
2023-08-28 08:48:18 -0700

    LPS-187437 Fix package

--- modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/spi/listener/CTOnDemandUserEventListener.java
+++ modules/apps/change-tracking/change-tracking-service/src/main/java/com/liferay/change/tracking/internal/spi/listener/CTOnDemandUserEventListener.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.change.tracking.internal.on.demand.user;
+package com.liferay.change.tracking.internal.spi.listener;
 
 import com.liferay.change.tracking.model.CTCollection;
 import com.liferay.change.tracking.service.CTCollectionLocalService;
