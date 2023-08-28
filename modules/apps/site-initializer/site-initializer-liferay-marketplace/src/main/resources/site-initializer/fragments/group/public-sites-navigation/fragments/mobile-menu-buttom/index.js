074607949cd5071fe0577f5ceb970b4f9bb74572
Joana Grego <joana.lima@liferay.com>
2023-08-28 15:32:28 -0300

    LPS-193674 Add menu buttom fragment

---
+++ modules/apps/site-initializer/site-initializer-liferay-marketplace/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/mobile-menu-buttom/index.js
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+/* eslint-disable no-undef */
+
+const menuButton = fragmentElement.querySelector('.nav-menu-buttom');
+const menuDisplay = ['show-menu', 'hidden-menu'];
+const menuIcon = fragmentElement.querySelector('#button-menu-icon');
+window.onload = () => {
+	const menuDisplayPages = document.querySelector('.nav-menu-pages');
+
+	menuButton.addEventListener('click', () => {
+		menuDisplay.forEach((cssClass) => {
+			menuDisplayPages.classList.toggle(cssClass);
+		});
+	});
+
+	menuButton.addEventListener('blur', () => {
+		if (menuDisplayPages.classList.contains('show-menu')) {
+			menuDisplay.forEach((cssClass) => {
+				menuDisplayPages.classList.toggle(cssClass);
+			});
+
+			backgroundIconDisplay.forEach((cssClass) => {
+				menuIcon.classList.toggle(cssClass);
+			});
+		}
+	});
+};
