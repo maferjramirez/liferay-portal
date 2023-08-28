338e245c0d829a930b9bfada8df3843765a557e0
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-08-28 18:53:55 -0300

    LPS-194720 Create a filter to check if MFA Policy is enabled in web module

---
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-web/src/main/java/com/liferay/multi/factor/authentication/web/filter/MFAFilter.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.web.filter;
+
+import com.liferay.multi.factor.authentication.web.internal.policy.MFAPolicy;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.BaseFilter;
+import com.liferay.portal.kernel.servlet.TryFilter;
+import com.liferay.portal.kernel.util.DigesterUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+
+@Component(
+	property = {
+		"after-filter=MFA Servlet Filter", "dispatcher=FORWARD",
+		"dispatcher=REQUEST", "servlet-context-name=",
+		"servlet-filter-name=MFA Filter", "url-pattern=/*"
+	},
+	service = Filter.class
+)
+public class MFAFilter
+	extends BaseFilter implements TryFilter {
+
+
+
+	@Override
+	public Object doFilterTry(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		 String path = (String) httpServletRequest.getAttribute(WebKeys.INVOKER_FILTER_URI);
+
+		 boolean mfaEnabled = _mfaPolicy.isMFAEnabled(GetterUtil.getLong(httpServletRequest.getAttribute(WebKeys.COMPANY_ID)));
+		 
+		 if (path.contains("/c/portal/update_password")) {
+			 HttpSession httpSession = httpServletRequest.getSession();
+			 httpSession.setAttribute(WebKeys.MFA_ENABLED, mfaEnabled);
+		 }
+
+		return true;
+	}
+
+	@Override
+	public boolean isFilterEnabled() {
+		return true;
+	}
+
+	@Activate
+	protected void activate(
+		BundleContext bundleContext, Map<String, Object> properties) {
+	}
+
+	@Override
+	protected Log getLog() {
+		return _log;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		MFAFilter.class);
+
+
+	@Reference
+	private MFAPolicy _mfaPolicy;
+
+	@Reference
+	private Portal _portal;
+
+}
