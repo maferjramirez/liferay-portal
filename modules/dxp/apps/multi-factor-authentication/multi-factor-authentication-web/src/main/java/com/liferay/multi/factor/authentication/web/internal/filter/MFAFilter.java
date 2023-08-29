8cef8494bf658029416cde882d498ea5ed965230
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-08-29 10:51:47 -0300

    LPS-194720 Add missing author

--- modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-web/src/main/java/com/liferay/multi/factor/authentication/web/internal/filter/MFAFilter.java
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-web/src/main/java/com/liferay/multi/factor/authentication/web/internal/filter/MFAFilter.java
@@ -35,6 +35,9 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+/**
+ * @author Mirna Gama
+ */
 @Component(
 	property = {
 		"after-filter=MFA Servlet Filter", "dispatcher=FORWARD",
