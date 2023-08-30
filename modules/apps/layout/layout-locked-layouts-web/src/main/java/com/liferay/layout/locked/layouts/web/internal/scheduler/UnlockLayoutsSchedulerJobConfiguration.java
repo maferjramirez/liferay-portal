47ed938c9ebb0ea68522231d9b1750c3fccc4d28
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2023-08-30 17:03:32 +0200

    LPS-194406 Adds a scheduler job configuration to trigger the unlock layouts job

---
+++ modules/apps/layout/layout-locked-layouts-web/src/main/java/com/liferay/layout/locked/layouts/web/internal/scheduler/UnlockLayoutsSchedulerJobConfiguration.java
@@ -0,0 +1,90 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.locked.layouts.web.internal.scheduler;
+
+import com.liferay.layout.locked.layouts.web.internal.configuration.LockedLayoutsConfiguration;
+import com.liferay.layout.manager.LayoutLockManager;
+import com.liferay.petra.function.UnsafeConsumer;
+import com.liferay.petra.function.UnsafeRunnable;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.scheduler.SchedulerJobConfiguration;
+import com.liferay.portal.kernel.scheduler.TimeUnit;
+import com.liferay.portal.kernel.scheduler.TriggerConfiguration;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(
+	configurationPid = "com.liferay.layout.locked.layouts.web.internal.configuration.LockedLayoutsConfiguration",
+	service = SchedulerJobConfiguration.class
+)
+public class UnlockLayoutsSchedulerJobConfiguration
+	implements SchedulerJobConfiguration {
+
+	@Override
+	public UnsafeConsumer<Long, Exception>
+		getCompanyJobExecutorUnsafeConsumer() {
+
+		return _getCompanyJobExecutorUnsafeConsumer();
+	}
+
+	@Override
+	public UnsafeRunnable<Exception> getJobExecutorUnsafeRunnable() {
+		return () -> _companyLocalService.forEachCompanyId(
+			_getCompanyJobExecutorUnsafeConsumer());
+	}
+
+	@Override
+	public TriggerConfiguration getTriggerConfiguration() {
+		return TriggerConfiguration.createTriggerConfiguration(
+			_lockedLayoutsConfiguration.lockReviewFrequency(), TimeUnit.MINUTE);
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_lockedLayoutsConfiguration = ConfigurableUtil.createConfigurable(
+			LockedLayoutsConfiguration.class, properties);
+	}
+
+	private UnsafeConsumer<Long, Exception>
+		_getCompanyJobExecutorUnsafeConsumer() {
+
+		return companyId -> {
+			LockedLayoutsConfiguration lockedLayoutsConfiguration =
+				_configurationProvider.getCompanyConfiguration(
+					LockedLayoutsConfiguration.class, companyId);
+
+			if (!lockedLayoutsConfiguration.allowAutomaticUnlockingProcess()) {
+				return;
+			}
+
+			_layoutLockManager.unlockLayouts(
+				companyId, lockedLayoutsConfiguration.timeWithoutAutosave());
+		};
+	}
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	@Reference
+	private LayoutLockManager _layoutLockManager;
+
+	private volatile LockedLayoutsConfiguration _lockedLayoutsConfiguration;
+
+}
