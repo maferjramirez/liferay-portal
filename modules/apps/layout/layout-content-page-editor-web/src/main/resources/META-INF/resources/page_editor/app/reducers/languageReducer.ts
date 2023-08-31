73776f6ffdc98a69deea0c27c32e74d1cb1e10a7
Pablo Molina <contact@pablomolina.me>
2023-08-31 12:26:00 +0200

    LPS-195022 Migrate languageReducer to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/languageReducer.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/languageReducer.ts
@@ -3,13 +3,15 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import * as TYPES from '../actions/types';
+import {UPDATE_LANGUAGE_ID} from '../actions/types';
+import updateLanguageId from '../actions/updateLanguageId';
 
-export const INITIAL_STATE = 'en-US';
-
-export default function languageReducer(languageId = INITIAL_STATE, action) {
+export default function languageReducer(
+	languageId: string = 'en-US',
+	action: ReturnType<typeof updateLanguageId>
+) {
 	switch (action.type) {
-		case TYPES.UPDATE_LANGUAGE_ID:
+		case UPDATE_LANGUAGE_ID:
 			return action.languageId;
 
 		default:
