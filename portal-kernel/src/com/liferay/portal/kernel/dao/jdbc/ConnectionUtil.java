d4c947c07e86857413da0424e24c8b092c4c71f7
Julius Lee <meng-huan.lee@liferay.com>
2023-08-31 15:44:03 -0700

    LPS-195103 Move ConnectionUtil to kernel prepping next

--- portal-kernel/src/com/liferay/portal/kernel/dao/jdbc/ConnectionUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/dao/jdbc/ConnectionUtil.java
@@ -3,9 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.dao.jdbc.spring;
+package com.liferay.portal.kernel.dao.jdbc;
 
-import com.liferay.portal.kernel.dao.jdbc.CurrentConnectionUtil;
 import com.liferay.portal.kernel.util.ProxyUtil;
 
 import java.lang.reflect.InvocationHandler;
