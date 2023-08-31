f77485b1dbc4638076e63ddad8c28eaec1d6f400
Pablo Molina <contact@pablomolina.me>
2023-08-31 12:35:02 +0200

    LPS-195022 Migrate restrictedItemIdsReducer to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/restrictedItemIdsReducer.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/restrictedItemIdsReducer.ts
@@ -3,19 +3,21 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import * as TYPES from '../actions/types';
+import duplicateItem from '../actions/duplicateItem';
+import {InitAction} from '../actions/init';
+import {DUPLICATE_ITEM, INIT} from '../actions/types';
 
-export const INITIAL_STATE = [];
+export const INITIAL_STATE = new Set<string>();
 
 export default function restrictedItemIdsReducer(
 	restrictedItemIds = INITIAL_STATE,
-	action
+	action: InitAction | ReturnType<typeof duplicateItem>
 ) {
 	switch (action.type) {
-		case TYPES.INIT:
+		case INIT:
 			return new Set(restrictedItemIds);
 
-		case TYPES.DUPLICATE_ITEM:
+		case DUPLICATE_ITEM:
 			return new Set(action.restrictedItemIds);
 
 		default:
