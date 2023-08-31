49f12a6ac068462c64c78fcebbdd7984107b595b
Pablo Molina <contact@pablomolina.me>
2023-08-31 12:17:53 +0200

    LPS-195022 Migrate collectionsReducer to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/collectionsReducer.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/collectionsReducer.ts
@@ -3,9 +3,18 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import addFragmentComposition from '../actions/addFragmentComposition';
 import {ADD_FRAGMENT_COMPOSITION} from '../actions/types';
 
-export default function collectionsReducer(collections = [], action) {
+interface Collection {
+	fragmentCollectionId: string;
+	name: string;
+}
+
+export default function collectionsReducer(
+	collections: Collection[] = [],
+	action: ReturnType<typeof addFragmentComposition>
+) {
 	switch (action.type) {
 		case ADD_FRAGMENT_COMPOSITION: {
 			const composition = action.fragmentComposition;
