d728f529917b0ba6c38b8d707e571dabc5c960cb
Pablo Molina <contact@pablomolina.me>
2023-08-31 12:36:35 +0200

    LPS-195022 Migrate selectedViewportSizeReducer to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/selectedViewportSizeReducer.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/selectedViewportSizeReducer.ts
@@ -3,13 +3,13 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import switchViewportSize from '../actions/switchViewportSize';
 import {SWITCH_VIEWPORT_SIZE} from '../actions/types';
-
-export const INITIAL_STATE = 'desktop';
+import {ViewportSize} from '../config/constants/viewportSizes';
 
 export default function selectedViewportSizeReducer(
-	viewportStatus = INITIAL_STATE,
-	action
+	viewportStatus: ViewportSize = 'desktop',
+	action: ReturnType<typeof switchViewportSize>
 ) {
 	if (action.type === SWITCH_VIEWPORT_SIZE) {
 		return action.size;
