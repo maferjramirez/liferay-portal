94d7fbfa0cb801577239b6dc6958a7ca5c998b61
Pablo Molina <contact@pablomolina.me>
2023-08-31 12:28:14 +0200

    LPS-195022 Migrate showResolvedCommentsReducer to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/showResolvedCommentsReducer.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/showResolvedCommentsReducer.ts
@@ -3,13 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import toggleShowResolvedComments from '../actions/toggleShowResolvedComments';
 import {TOGGLE_SHOW_RESOLVED_COMMENTS} from '../actions/types';
 
-export const INITIAL_STATE = false;
-
 export default function showResolvedCommentsReducer(
-	toggleResolvedComments = INITIAL_STATE,
-	action
+	toggleResolvedComments = false,
+	action: ReturnType<typeof toggleShowResolvedComments>
 ) {
 	switch (action.type) {
 		case TOGGLE_SHOW_RESOLVED_COMMENTS:
