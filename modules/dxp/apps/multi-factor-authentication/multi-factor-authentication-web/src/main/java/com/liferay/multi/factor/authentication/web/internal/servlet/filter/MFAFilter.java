885a858ad23c41910fe9afffbc78da346f996bc0
Brian Chan <brian.chan@liferay.com>
2023-09-05 06:31:20 -0700

    LPS-194720 Wrong dir

--- modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-web/src/main/java/com/liferay/multi/factor/authentication/web/internal/servlet/filter/MFAFilter.java
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-web/src/main/java/com/liferay/multi/factor/authentication/web/internal/servlet/filter/MFAFilter.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.multi.factor.authentication.web.internal.filter;
+package com.liferay.multi.factor.authentication.web.internal.servlet.filter;
 
 import com.liferay.multi.factor.authentication.web.internal.policy.MFAPolicy;
 import com.liferay.portal.kernel.log.Log;
